8c3265e8926a7ffd3430fdadeff3d1fe2f2608bd
xMalware <Aurelian User@Aurelian>
2016-05-02 17:51:20 +0200

    First commit :o (w/ Docker factories)

---
+++ src/fr/badblock/common/docker/factories/GameAliveFactory.java
@@ -0,0 +1,20 @@
+package fr.badblock.common.docker.factories;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter public class GameAliveFactory {
+
+	private String 		name;
+	private boolean 	isJoinable;
+	private int			players;
+	private int			slots;
+	
+	public GameAliveFactory(String name, boolean isJoinable, int players, int slots) {
+		this.setName(name);
+		this.setJoinable(isJoinable);
+		this.setPlayers(players);
+		this.setSlots(slots);
+	}
+	
+}
