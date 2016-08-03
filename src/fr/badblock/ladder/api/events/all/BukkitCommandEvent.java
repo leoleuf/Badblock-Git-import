821ab5056a8c6eb0f063f05b3975e01e427d68d8
xMalware <Aurelian User@Aurelian>
2016-08-03 13:50:14 +0100

    Added player into BukkitCommandEvent

--- src/fr/badblock/ladder/api/events/all/BukkitCommandEvent.java
+++ src/fr/badblock/ladder/api/events/all/BukkitCommandEvent.java
@@ -1,14 +1,24 @@
 package fr.badblock.ladder.api.events.all;
 
 import fr.badblock.ladder.api.commands.Command;
+import fr.badblock.ladder.api.entities.Player;
 import lombok.Getter;
 
 public class BukkitCommandEvent extends CommandEvent {
+	
 	@Getter private final Command command;
+	@Getter private final Player player;
+
+	public BukkitCommandEvent(Command command, Player player, String message) {
+		super(player, message);
+		this.player = player;
+		this.command = command;
+	}
 	
 	public BukkitCommandEvent(Command command, String message) {
 		super(null, message);
-		
+		this.player = null;
 		this.command = command;
 	}
+	
 }
