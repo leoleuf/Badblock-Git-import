cd0dc9f13c08c2a5abe24988f62e69593e76666c
LeLanN <audrandoublet@gmail.com>
2016-09-04 21:33:28 +0200

    Okay ... thanks mojang

--- src/fr/badblock/minecraftserver/BadblockConfig.java
+++ src/fr/badblock/minecraftserver/BadblockConfig.java
@@ -8,7 +8,6 @@ public class BadblockConfig {
 	public static class BadblockConfigTileEntities {
 		public boolean tickBeacon 		 = true;
 		public boolean tickBrewingStand  = true;
-		public boolean tickEnchantTable  = true;
 		public boolean tickFurnace 		 = true;
 		public boolean tickHopper 		 = true;
 		public boolean tickLightDetector = true;
--- src/net/minecraft/server/v1_8_R3/TileEntityEnchantTable.java
+++ src/net/minecraft/server/v1_8_R3/TileEntityEnchantTable.java
@@ -1,22 +1,7 @@
 package net.minecraft.server.v1_8_R3;
 
-import java.util.Random;
+public class TileEntityEnchantTable extends TileEntity implements ITileEntityContainer {
 
-import fr.badblock.minecraftserver.BadblockConfig;
-
-public class TileEntityEnchantTable extends TileEntity implements IUpdatePlayerListBox, ITileEntityContainer {
-
-    public int a;
-    public float f;
-    public float g;
-    public float h;
-    public float i;
-    public float j;
-    public float k;
-    public float l;
-    public float m;
-    public float n;
-    private static Random o = new Random();
     private String p;
 
     public TileEntityEnchantTable() {}
@@ -35,72 +20,6 @@ public class TileEntityEnchantTable extends TileEntity implements IUpdatePlayerL
             this.p = nbttagcompound.getString("CustomName");
         }
 
-    }
-
-    public void c() {
-        this.k = this.j;
-        this.m = this.l;
-        EntityHuman entityhuman = this.world.findNearbyPlayer((double) ((float) this.position.getX() + 0.5F), (double) ((float) this.position.getY() + 0.5F), (double) ((float) this.position.getZ() + 0.5F), 3.0D);
-
-        if (entityhuman != null) {
-            double d0 = entityhuman.locX - (double) ((float) this.position.getX() + 0.5F);
-            double d1 = entityhuman.locZ - (double) ((float) this.position.getZ() + 0.5F);
-
-            this.n = (float) MathHelper.b(d1, d0);
-            this.j += 0.1F;
-            if (this.j < 0.5F || TileEntityEnchantTable.o.nextInt(40) == 0) {
-                float f = this.h;
-
-                do {
-                    this.h += (float) (TileEntityEnchantTable.o.nextInt(4) - TileEntityEnchantTable.o.nextInt(4));
-                } while (f == this.h);
-            }
-        } else {
-            this.n += 0.02F;
-            this.j -= 0.1F;
-        }
-
-        while (this.l >= 3.1415927F) {
-            this.l -= 6.2831855F;
-        }
-
-        while (this.l < -3.1415927F) {
-            this.l += 6.2831855F;
-        }
-
-        while (this.n >= 3.1415927F) {
-            this.n -= 6.2831855F;
-        }
-
-        while (this.n < -3.1415927F) {
-            this.n += 6.2831855F;
-        }
-
-        float f1;
-
-        for (f1 = this.n - this.l; f1 >= 3.1415927F; f1 -= 6.2831855F) {
-            ;
-        }
-
-        while (f1 < -3.1415927F) {
-            f1 += 6.2831855F;
-        }
-
-        this.l += f1 * 0.4F;
-        this.j = MathHelper.a(this.j, 0.0F, 1.0F);
-        ++this.a;
-        this.g = this.f;
-        float f2 = (this.h - this.f) * 0.4F;
-        float f3 = 0.2F;
-
-        f2 = MathHelper.a(f2, -f3, f3);
-        this.i += (f2 - this.i) * 0.9F;
-        this.f += this.i;
-    }
-    
-    @Override
-    public boolean mustUpdatePlayerListBox(){
-    	return BadblockConfig.config.tileEntities.tickEnchantTable;
     }
 
     public String getName() {
