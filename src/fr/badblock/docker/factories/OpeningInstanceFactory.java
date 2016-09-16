c20d09ac077738eb043be20b30a5b7b5f6a808d9
xMalware <xmalware2@gmail.com>
2016-09-16 22:20:40 +0200

    Facteur y

---
+++ src/fr/badblock/docker/factories/OpeningInstanceFactory.java
@@ -0,0 +1,63 @@
+package fr.badblock.docker.factories;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OpeningInstanceFactory {
+
+	private long id;
+	private String ip;
+	private long port;
+	private int slots;
+	private String bungeeName;
+	private String screenName;
+	private String world;
+	private String system;
+	private long ram;
+	private String command;
+	private int nettyThreads;
+	private int parallelThreads;
+	private long timeExecuted;
+	private String mode;
+	private String jarServer;
+	private String logFolder;
+	private String logFile;
+	private boolean random;
+	private String toWorld;
+	private InputStream inputStream;
+	private OutputStream outputStream;
+	private Process process;
+	private String rabbitHostname;
+	private String ladderIp;
+	private int ladderPort;
+
+	public OpeningInstanceFactory(long id, String ip, long port, int slots, String bungeeName, String screenName,
+			String world, String systemName, long ram, String command, int nettyThreads, int parallelThreads,
+			String mode, String jarServer, String logFolder, String logFile, boolean random, String toWorld) {
+		setId(id);
+		setIp(ip);
+		setPort(port);
+		setSlots(slots);
+		setBungeeName(bungeeName);
+		setScreenName(screenName);
+		setWorld(world);
+		setSystem(system);
+		setRam(ram);
+		setCommand(command);
+		setNettyThreads(nettyThreads);
+		setParallelThreads(parallelThreads);
+		setTimeExecuted(System.currentTimeMillis());
+		setMode(mode);
+		setJarServer(jarServer);
+		setLogFolder(logFolder);
+		setLogFile(logFile);
+		setRandom(random);
+		setToWorld(toWorld);
+	}
+
+}
