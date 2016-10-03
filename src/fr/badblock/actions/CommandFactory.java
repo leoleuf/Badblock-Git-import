57dbd47a2d4f25dc7777e05250c4a4905d3144b5
xMalware <xmalware2@gmail.com>
2016-10-03 18:04:32 +0200

    CommandFactory

---
+++ src/fr/badblock/actions/CommandFactory.java
@@ -0,0 +1,12 @@
+package fr.badblock.actions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class CommandFactory {
+
+	private String playerName;
+	private String command;
+	
+}
