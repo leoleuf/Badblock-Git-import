9ffefc67905ab427d666eb4fc0b63d69f134b7c2
xMalware <xmalware2@gmail.com>
2016-10-14 17:20:46 +0200

    fix infinite ttl

--- src/fr/badblock/rabbitconnector/RabbitMessage.java
+++ src/fr/badblock/rabbitconnector/RabbitMessage.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 	private long   expire = -1L;
 	private String message;
-	
+
 	public RabbitMessage(long ttl, String message) {
 		if (ttl > 0) setExpire(System.currentTimeMillis() + ttl);
 		setMessage(message);
@@ -18,7 +18,7 @@ import lombok.Setter;
 	}
 	
 	public boolean isExpired() {
-		return getExpire() != -1L && getExpire() < System.currentTimeMillis();
+		return getExpire() == -1L || getExpire() < System.currentTimeMillis();
 	}
 	
 	public String toJson() {
