56ea8d4485f899eabbdb3d8e8a407eca176a789f
LeLanN <audrandoublet@gmail.com>
2016-10-22 03:15:25 +0200

    private -> public

--- src/fr/badblock/commons/data/PlayerGameData.java
+++ src/fr/badblock/commons/data/PlayerGameData.java
@@ -1,25 +1,21 @@
 package fr.badblock.commons.data;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
-import com.google.gson.JsonObject;
 
 import fr.badblock.commons.data.i18n.Locale;
 
 public class PlayerGameData {
 	public Locale								  		  locale	       = Locale.FRENCH_FRANCE;
 
-	private int  				 						  badcoins     	   = 0;
-	public  int  				 						  shopPoints       = 0;
-	private int  				 						  level	     	   = 1;
-	private long 										  xp		       = 0L;
+	public int  				 						  badcoins     	   = 0;
+	public int  				 						  shopPoints       = 0;
+	public int  				 						  level	     	   = 1;
+	public long 										  xp		       = 0L;
 	//TODO private List<PlayerBooster>					  		  boosters		   = new ArrayList<>();
-	private Map<String, Integer> 						  kits 		 	   = new ConcurrentHashMap<>();
-	private Map<String, String>							  lastUsedKits 	   = new ConcurrentHashMap<>();
+	public Map<String, Integer> 						  kits 		 	   = new ConcurrentHashMap<>();
+	public Map<String, String>							  lastUsedKits 	   = new ConcurrentHashMap<>();
 	//TODO private Map<String, PlayerAchievementState> 		  achievements 	   = new ConcurrentHashMap<>();
 
-	private Map<String, Map<String, Double>> 			  stats   	 	   = new ConcurrentHashMap<>();
+	public Map<String, Map<String, Double>> 			  stats   	 	   = new ConcurrentHashMap<>();
 }
