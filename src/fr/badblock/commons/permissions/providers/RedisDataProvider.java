98c01fe882723cdae1093ca9e5a33345b1f4c0a8
LeLanN <audrandoublet@gmail.com>
2016-10-22 03:37:35 +0200

    Better data handling

--- src/fr/badblock/commons/permissions/providers/RedisDataProvider.java
+++ src/fr/badblock/commons/permissions/providers/RedisDataProvider.java
@@ -1,7 +1,11 @@
 package fr.badblock.commons.permissions.providers;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import fr.badblock.commons.data.PlayerData;
@@ -15,6 +19,8 @@ public class RedisDataProvider implements PermissionProvider {
 	private String[]	 locations;
 	private RedisService service;
 	
+	private Map<String, JsonObject> loadedPlayers = new HashMap<String, JsonObject>();
+	
 	public RedisDataProvider(String[] locations, String host, int port, String password) {
 		this.locations = locations;
 		this.service   = RedisConnector.getInstance().newService("permissions_fetcher", host, port, password);
@@ -27,14 +33,51 @@ public class RedisDataProvider implements PermissionProvider {
 	
 	@Override
 	public void loadGroups(Callback<Map<String, PermissibleGroup>> callback) {
-		service.getAsyncObject("badblock_permissions", callback);
+		service.getAsyncObject("badblock.permissions", callback);
 	}
 
 	public void loadPlayer(String name, Callback<JsonObject> callback) {
-		service.getAsyncObject("badblock_player_" + name.toLowerCase(), callback);
+		service.getAsyncObject("badblock.player." + name.toLowerCase(), new Callback<JsonObject>() {
+			@Override
+			public void done(JsonObject result, Throwable error) {
+				if(error != null){
+					loadedPlayers.put(name, result);
+				}
+				
+				callback.done(result, error);
+			}
+		});
+	}
+	
+	public void sendFullPlayer(String name, JsonObject object){
+		service.setAsync("badblock.player." + name, object.toString());
 	}
 	
 	public void sendPlayer(String name, PlayerData data){
 		
+		JsonObject toSend = new Gson().toJsonTree(data).getAsJsonObject();
+		
+		if(loadedPlayers.containsKey(name.toLowerCase())){
+			toSend = addObjectInObject(loadedPlayers.get(name.toLowerCase()), toSend);
+			loadedPlayers.remove( name.toLowerCase() );
+		}
+		
+		sendFullPlayer(name, toSend);
+		
+	}
+	
+	private JsonObject addObjectInObject(JsonObject base, JsonObject toAdd){
+		for(final Entry<String, JsonElement> entry : toAdd.entrySet()){
+			String key = entry.getKey();
+			JsonElement element = toAdd.get(key);
+
+			if(!base.has(key) || !element.isJsonObject()){
+				base.add(key, element);
+			} else {
+				addObjectInObject(base.get(key).getAsJsonObject(), element.getAsJsonObject());
+			}
+		}
+		
+		return base;
 	}
 }
