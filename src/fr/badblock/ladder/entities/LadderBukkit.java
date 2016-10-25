fe45718f180c0fed7598983b9f1f1f684764c639
xMalware <xmalware2@gmail.com>
2016-10-25 07:11:15 +0200

    c

--- src/fr/badblock/ladder/entities/LadderBukkit.java
+++ src/fr/badblock/ladder/entities/LadderBukkit.java
@@ -116,13 +116,13 @@ public class LadderBukkit implements Bukkit, PacketHandler {
 						final PermissiblePlayer permPlayer = permissiblePlayer;
 						PermissibleGroup permissibleGroup = (PermissibleGroup) permissiblePlayer.getParent();
 						if (permissibleGroup.isStaff()) {
-							permissiblePlayer.getPermissions().forEach(permission -> permPlayer.addPermission(permission));
+							permissiblePlayer.getPermissions().forEach(permission -> permPlayer.permissions.add(permission));
 							permissiblePlayer.removeParent(permissiblePlayer.getParent());
 						}
 						permPlayer.getAlternateGroups().entrySet().stream().filter(group -> LadderPermissionManager.getInstance().getGroup(group.getKey()) != null && LadderPermissionManager.getInstance().getGroup(group.getKey()).isStaff()).forEach(group -> {
 							PermissibleGroup groupe = LadderPermissionManager.getInstance().getGroup(group.getKey());
 							if (groupe == null) return;
-							groupe.getPermissions().forEach(permission -> permPlayer.addPermission(permission));
+							groupe.getPermissions().forEach(permission -> permPlayer.permissions.add(permission));
 							permPlayer.removeParent(group.getKey());
 						});
 					}
