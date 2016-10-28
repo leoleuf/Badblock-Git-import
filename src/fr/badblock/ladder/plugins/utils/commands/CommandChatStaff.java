1956af0072316669d717f51c241af5f6239a7537
xMalware <xmalware2@gmail.com>
2016-10-28 04:56:09 +0100

    fix

--- src/fr/badblock/ladder/plugins/utils/commands/CommandChatStaff.java
+++ src/fr/badblock/ladder/plugins/utils/commands/CommandChatStaff.java
@@ -22,7 +22,7 @@ public class CommandChatStaff extends Command {
 		if (!(sender instanceof Player)) return;
 		Player player = (Player) sender;
 		PermissiblePlayer permissiblePlayer = (PermissiblePlayer) player.getAsPermissible();
-		String message = "§6§l[CS] " + permissiblePlayer.getDisplayName() + player.getName() + "§7 > §7" + ChatColor.AQUA + StringUtils.join(args, " ");
+		String message = "§6§l[CS] &r" + ChatColor.translateAlternateColorCodes('&', permissiblePlayer.getDisplayName()) + "§7 > §7" + ChatColor.AQUA + StringUtils.join(args, " ");
 		for(Player plo : Ladder.getInstance().getOnlinePlayers()){
 			if(plo.hasPermission("ladder.command.chatstaff"))
 				plo.sendMessage(message);
