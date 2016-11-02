aa5b75812a3389467d80ccccfcbf91376a99f7c1
xMalware <xmalware2@gmail.com>
2016-11-02 02:41:52 +0100

    DockerUpdateFactory

---
+++ src/fr/badblock/docker/factories/DockerUpdateFactory.java
@@ -0,0 +1,12 @@
+package fr.badblock.docker.factories;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class DockerUpdateFactory {
+
+	public String hostname;
+	public String data;
+	
+}
