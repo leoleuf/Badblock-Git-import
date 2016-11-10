027109090d4bc36b4b4943f593e72576b42b6295
xMalware <xmalware2@gmail.com>
2016-11-10 23:26:19 +0100

    yt

--- src/fr/badblock/bungee/utils/AdminBroadcastListener.java
+++ src/fr/badblock/bungee/utils/AdminBroadcastListener.java
@@ -4,6 +4,7 @@ import fr.badblock.bungee.rabbitconnector.RabbitListener;
 import fr.badblock.bungee.rabbitconnector.RabbitListenerType;
 import fr.badblock.ladder.bungee.LadderBungee;
 import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.api.ChatColor;
 
 public class AdminBroadcastListener extends RabbitListener {
 
@@ -13,7 +14,7 @@ public class AdminBroadcastListener extends RabbitListener {
 
 	@Override
 	public void onPacketReceiving(String body) {
-		BungeeCord.getInstance().broadcast(body);
+		BungeeCord.getInstance().broadcast(ChatColor.translateAlternateColorCodes('&', body));
 	}
 	
 }
