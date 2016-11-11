666d67e5e03e9f25952509d1db1ac6a2e5017727
xMalware <xmalware2@gmail.com>
2016-11-11 01:26:53 +0100

    gr

--- src/fr/badblock/protocol/packets/PacketPlayerPlace.java
+++ src/fr/badblock/protocol/packets/PacketPlayerPlace.java
@@ -1,7 +1,6 @@
 package fr.badblock.protocol.packets;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import fr.badblock.protocol.PacketHandler;
 import fr.badblock.protocol.buffers.ByteInputStream;
@@ -16,18 +15,18 @@ import lombok.NoArgsConstructor;
  */
 @Data@NoArgsConstructor@AllArgsConstructor
 public class PacketPlayerPlace implements Packet {
-	private UUID   uniqueId;
+	private String playerName;
 	private String serverName;
 
 	@Override
 	public void read(ByteInputStream input) throws IOException {
-		uniqueId	= input.readUUID();
+		playerName	= input.readUTF();
 		serverName  = input.readUTF();
 	}
 
 	@Override
 	public void write(ByteOutputStream output) throws IOException {
-		output.writeUUID(uniqueId);
+		output.writeUTF(playerName);
 		output.writeUTF(serverName);
 	}
 
