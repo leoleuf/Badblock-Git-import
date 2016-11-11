8af1b807bb40733574dd3768090022807076e2f9
xMalware <xmalware2@gmail.com>
2016-11-11 12:44:14 +0100

    m

--- src/fr/badblock/protocol/packets/PacketPlayerNickSet.java
+++ src/fr/badblock/protocol/packets/PacketPlayerNickSet.java
@@ -1,6 +1,7 @@
 package fr.badblock.protocol.packets;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import fr.badblock.protocol.PacketHandler;
 import fr.badblock.protocol.buffers.ByteInputStream;
@@ -12,17 +13,20 @@ import lombok.NoArgsConstructor;
 @Data@NoArgsConstructor@AllArgsConstructor
 public class PacketPlayerNickSet implements Packet {
 	private String 			  playerName;
+	private UUID			  uuid;
 	private String 			  nickName;
 
 	@Override
 	public void read(ByteInputStream input) throws IOException {
 		playerName = input.readUTF();
+		uuid = input.readUUID();
 		nickName = input.readUTF();
 	}
 
 	@Override
 	public void write(ByteOutputStream output) throws IOException {
 		output.writeUTF(playerName);
+		output.writeUUID(uuid);
 		output.writeUTF(nickName);
 	}
 
