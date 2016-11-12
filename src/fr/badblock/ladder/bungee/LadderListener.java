f71705e7162816e973df5ff1af9611db3f0e1296
xMalware <xmalware2@gmail.com>
2016-11-12 01:39:38 +0100

    debug pour voir d'où viennent les blocages de queue

--- src/fr/badblock/ladder/bungee/LadderListener.java
+++ src/fr/badblock/ladder/bungee/LadderListener.java
@@ -50,13 +50,16 @@ public class LadderListener implements Listener {
 		if(LadderBungee.getInstance().getPlayer(e.getPlayer()) != null){
 			ProxiedPlayer player = BungeeCord.getInstance().getPlayer(e.getPlayer());
 			if (player == null || !player.isConnected()) {
-				LadderBungee.getInstance().players.remove(LadderBungee.getInstance().getPlayer(e.getPlayer()));
+				LadderBungee.getInstance().players.remove(e.getPlayer());
+				LadderBungee.getInstance().players.remove(e.getPlayer().toLowerCase());
 				LadderBungee.getInstance().byName.remove(e.getPlayer());
+				LadderBungee.getInstance().byName.remove(e.getPlayer().toLowerCase());
+			}else{
+				e.getDone().done(new Result(null, ChatColor.RED + "Vous êtes déjà connecté sur BadBlock!"), null);
 			}
-			e.getDone().done(new Result(null, ChatColor.RED + "Vous êtes déjà connecté sur BadBlock !"), null);
 			return;
 		}
-
+		System.out.println("AsyncDataLoadRequest");
 		PacketPlayerLogin packet = new PacketPlayerLogin(e.getPlayer(), e.getHandler().getAddress());
 		LadderBungee.getInstance().handle(packet, e.getDone());
 		LadderBungee.getInstance().getClient().sendPacket(packet);
@@ -64,6 +67,7 @@ public class LadderListener implements Listener {
 
 	@EventHandler
 	public void onJoin(PostLoginEvent e){
+		System.out.println("PostLoginEvent");
 		Player player = LadderBungee.getInstance().getPlayer(e.getPlayer().getName());
 		PacketPlayerJoin packet = new PacketPlayerJoin(e.getPlayer().getName(), player == null ? e.getPlayer().getName() : player.getNickNamee(), e.getPlayer().getUniqueId(), e.getPlayer().getAddress());
 		LadderBungee.getInstance().handle(packet, true);
@@ -84,7 +88,7 @@ public class LadderListener implements Listener {
 
 	@EventHandler
 	public void onQuit(LoginFailEvent e){
-		if(e.getHandler().getName() == null) return;
+		System.out.println("LoginFailEvent");
 
 		PacketPlayerQuit packet = new PacketPlayerQuit(e.getHandler().getName(), null);
 		LadderBungee.getInstance().handle(packet, true);
@@ -93,6 +97,7 @@ public class LadderListener implements Listener {
 
 	@EventHandler
 	public void onQuit(PlayerDisconnectEvent e){
+		System.out.println("PlayerDisconnectEvent");
 		PacketPlayerQuit packet = new PacketPlayerQuit(e.getPlayer().getName(), null);
 		LadderBungee.getInstance().handle(packet, true);
 		LadderBungee.getInstance().getClient().sendPacket(packet);
@@ -100,6 +105,7 @@ public class LadderListener implements Listener {
 
 	@EventHandler(priority=EventPriority.HIGHEST)
 	public void onServerSwitch(ServerConnectEvent e){
+		System.out.println("ServerConnectEvent");
 		Player player = LadderBungee.getInstance().getPlayer(e.getPlayer().getName());
 		if (player != null) {
 			try {
@@ -150,6 +156,7 @@ public class LadderListener implements Listener {
 
 	@EventHandler
 	public void onFail(ServerConnectionFailEvent e){
+		System.out.println("ServerConnectionFailEvent");
 		if(!e.isKick()){
 			LadderBungee.getInstance().getClient().sendPacket(new PacketPlayerPlace(e.getPlayer().getName(), e.getFallback().getName()));
 		}
