3936705bfc1f9385c3e252702e80f3db7863ec7f
xMalware <xmalware2@gmail.com>
2016-11-28 00:30:02 +0100

    r

--- src/fr/badblock/bungee/utils/BungeeUtils.java
+++ src/fr/badblock/bungee/utils/BungeeUtils.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import fr.badblock.ladder.bungee.LadderBungee;
 import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
+import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.event.ProxyReloadEvent;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -19,6 +22,7 @@ import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
 
 public class BungeeUtils extends Plugin implements Listener{
 	public static ConfigurationProvider cp = ConfigurationProvider.getProvider(YamlConfiguration.class);
@@ -28,6 +32,8 @@ public class BungeeUtils extends Plugin implements Listener{
 
 	private int 		hubMaxPlayers;
 	private int 		loginMaxPlayers;
+	private long		timestampLimit;
+	private String		timestampReachLimit;
 	private ServerInfo	skeleton;
 
 	@Override
@@ -63,6 +69,8 @@ public class BungeeUtils extends Plugin implements Listener{
 		}
 		hubMaxPlayers = config.getInt("hubMaxPlayers", 100);
 		loginMaxPlayers = config.getInt("loginMaxPlayers", 100);
+		timestampLimit = config.getLong("timestampLimit", System.currentTimeMillis() / 1000L);
+		timestampReachLimit = config.getString("timestampReachLimit", "FINISHED");
 	}
 
 	@EventHandler
@@ -81,47 +89,74 @@ public class BungeeUtils extends Plugin implements Listener{
 		}
 	}
 
-	@EventHandler
-	public void onServerConnect(ServerConnectEvent e) {
-		if (e.getTarget() != null && e.getTarget().getName().equalsIgnoreCase( skeleton.getName() )) {
-			ServerInfo serverInfo = this.roundrobinLogin();
+	private String[] lastMotd;
 
-			if (serverInfo != null) {
-				e.setTarget(serverInfo);
+	@EventHandler (priority = EventPriority.HIGHEST)
+	public void onProxyPing(ProxyPingEvent e){
+		for (String l : LadderBungee.getInstance().getMotd().getMotd())
+			if (l.contains("@1")) {
+				lastMotd = LadderBungee.getInstance().getMotd().getMotd().clone();
 			}
-		} else if(e.getTarget() == null || (e.getTarget() != null && e.getTarget().getName().equals("lobby"))) {
-			ServerInfo serverInfo = this.roundrobinHub();
-
-			if (serverInfo != null) {
-				e.setTarget(serverInfo);
+		if (lastMotd == null) lastMotd = LadderBungee.getInstance().getMotd().getMotd();
+		if (lastMotd != null) {
+			List<String> strings = Arrays.asList(lastMotd.clone());
+			String[] newMotd = new String[lastMotd.length];
+			int i = -1;
+			for (String string : strings) {
+				i++;
+				newMotd[i] = string;
+				long time = timestampLimit - (System.currentTimeMillis() / 1000L);
+				if (time > 0) {
+					newMotd[i] = newMotd[i].replace("@1", TimeUnit.SECOND.toShort(time));
+				}else {
+					newMotd[i] = newMotd[i].replace("@1", timestampReachLimit);
+				}
 			}
-		}
+			LadderBungee.getInstance().getMotd().setMotd(newMotd);
+		} 
 	}
 
-	public ServerInfo roundrobinHub() {
-		List<ServerInfo> servers = new ArrayList<>();
-		for (ServerInfo serverInfo : BungeeCord.getInstance().getServers().values()) {
-			if (serverInfo == null) continue;
-			if (!serverInfo.getName().startsWith("hub")) continue;
-			//if (!lobbies.containsKey(serverInfo)) continue;
-			//if (lobbies.get(serverInfo) < System.currentTimeMillis()) continue;
-			if (serverInfo.getPlayers().size() >= hubMaxPlayers) continue;
-			servers.add(serverInfo);
-		}
-		return servers.get(new SecureRandom().nextInt(servers.size()));
-	}
+@EventHandler
+public void onServerConnect(ServerConnectEvent e) {
+	if (e.getTarget() != null && e.getTarget().getName().equalsIgnoreCase( skeleton.getName() )) {
+		ServerInfo serverInfo = this.roundrobinLogin();
 
-	private ServerInfo roundrobinLogin() {
-		List<ServerInfo> servers = new ArrayList<>();
-		for (ServerInfo serverInfo : BungeeCord.getInstance().getServers().values()) {
-			if (serverInfo == null) continue;
-			if (!serverInfo.getName().startsWith("login")) continue;
-			//if (!logins.containsKey(serverInfo)) continue;
-			//if (logins.get(serverInfo) < System.currentTimeMillis()) continue;
-			if (serverInfo.getPlayers().size() >= loginMaxPlayers) continue;
-			servers.add(serverInfo);
+		if (serverInfo != null) {
+			e.setTarget(serverInfo);
 		}
-		return servers.get(new SecureRandom().nextInt(servers.size()));
+	} else if(e.getTarget() == null || (e.getTarget() != null && e.getTarget().getName().equals("lobby"))) {
+		ServerInfo serverInfo = this.roundrobinHub();
+
+		if (serverInfo != null) {
+			e.setTarget(serverInfo);
+		}
 	}
+}
+
+public ServerInfo roundrobinHub() {
+	List<ServerInfo> servers = new ArrayList<>();
+	for (ServerInfo serverInfo : BungeeCord.getInstance().getServers().values()) {
+		if (serverInfo == null) continue;
+		if (!serverInfo.getName().startsWith("hub")) continue;
+		//if (!lobbies.containsKey(serverInfo)) continue;
+		//if (lobbies.get(serverInfo) < System.currentTimeMillis()) continue;
+		if (serverInfo.getPlayers().size() >= hubMaxPlayers) continue;
+		servers.add(serverInfo);
+	}
+	return servers.get(new SecureRandom().nextInt(servers.size()));
+}
+
+private ServerInfo roundrobinLogin() {
+	List<ServerInfo> servers = new ArrayList<>();
+	for (ServerInfo serverInfo : BungeeCord.getInstance().getServers().values()) {
+		if (serverInfo == null) continue;
+		if (!serverInfo.getName().startsWith("login")) continue;
+		//if (!logins.containsKey(serverInfo)) continue;
+		//if (logins.get(serverInfo) < System.currentTimeMillis()) continue;
+		if (serverInfo.getPlayers().size() >= loginMaxPlayers) continue;
+		servers.add(serverInfo);
+	}
+	return servers.get(new SecureRandom().nextInt(servers.size()));
+}
 
 }
