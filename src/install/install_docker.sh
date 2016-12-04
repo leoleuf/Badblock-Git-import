d477b601833a337e9be07b2681a94eedfaff068b
xMalware <xmalware2@gmail.com>
2016-12-04 23:02:09 +0100

    created script for updating to the latest OVH kernel (version4.8.8)

--- src/install/install_docker.sh
+++ src/install/install_docker.sh
@@ -18,9 +18,6 @@ nano /home/mc/Docker/configuration.json
 # Install Zabbix-Agent
 sh install_zabbixagent.sh
 # Update the kernel to 4.8.8
-cd /boot
-wget ftp://ftp.ovh.net/made-in-ovh/bzImage/4.8.8/System.map-4.8.8-xxxx-std-ipv6-64
-wget ftp://ftp.ovh.net/made-in-ovh/bzImage/4.8.8/bzImage-4.8.8-xxxx-std-ipv6-64
-update-grub
+sh ../upkeep/update_kernel4.8.8.sh
 # Reboot
 shutdown -r now
---
+++ src/upkeep/update_kernel4.8.8.sh
@@ -0,0 +1,4 @@
+cd /boot
+wget ftp://ftp.ovh.net/made-in-ovh/bzImage/4.8.8/System.map-4.8.8-xxxx-std-ipv6-64
+wget ftp://ftp.ovh.net/made-in-ovh/bzImage/4.8.8/bzImage-4.8.8-xxxx-std-ipv6-64
+update-grub
