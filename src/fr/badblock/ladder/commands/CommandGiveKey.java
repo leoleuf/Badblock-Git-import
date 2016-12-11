10e6d40a797f39cf1b3f9e9bea91ff15baf07a50
Audran <Audran@DESKTOP-MIJDA7D>
2016-12-11 10:25:37 +0100

    Le mec qui pousse 10 ans après parce qu'il est un peu con

---
+++ src/fr/badblock/ladder/commands/CommandGiveKey.java
@@ -0,0 +1,138 @@
+package fr.badblock.ladder.commands;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import fr.badblock.ladder.api.Ladder;
+import fr.badblock.ladder.api.chat.ChatColor;
+import fr.badblock.ladder.api.commands.Command;
+import fr.badblock.ladder.api.entities.CommandSender;
+import fr.badblock.ladder.api.entities.Player;
+import fr.badblock.ladder.api.utils.StringUtils;
+import fr.badblock.ladder.http.SlackMessage;
+import lombok.Data;
+
+public class CommandGiveKey extends Command {
+	public static final String username_regex = "^[a-zA-Z0-9_-]{3,15}$";
+
+	public CommandGiveKey() {
+		super("givekey", "ladder.command.givekey");
+	}
+
+	@Override
+	public void executeCommand(CommandSender sender, String[] args) {
+		if (args.length < 3) {
+			help(sender);
+		}
+
+		int id;
+
+		try {
+			id = Integer.parseInt(args[0]);
+		} catch(Exception e){
+			help(sender);
+			return;
+		}
+
+		List<String> players =  new ArrayList<>();
+
+		if(args[2].equals("-l")){
+			if(args.length < 4){
+				help(sender);
+				return;
+			}
+
+			try {
+				URL url = new URL("https://www.google.fr/?gws_rd=ssl");
+
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+				if(connection.getResponseCode() != 200){
+					throw new Exception("Error code: " + connection.getResponseCode());
+				}
+
+				BufferedReader reader = new BufferedReader( new InputStreamReader(connection.getInputStream()) );
+
+				String line = null;
+
+				while((line = reader.readLine()) != null)
+					if(line.matches(username_regex))
+						players.add(line);
+			} catch(Exception e){
+				e.printStackTrace();
+				sender.sendMessage(ChatColor.RED + "Improssible de lire la page : " + e.getMessage());
+
+				return;
+			}
+		} else {
+			for(int i=2;i<args.length;i++){
+				if(args[i].matches(username_regex))
+					players.add(args[i]);
+			}
+		}
+
+		if(players.size() == 0){
+			sender.sendMessage(ChatColor.RED + "Aucun joueur trouvé pour appliquer le give !");
+		} else {
+			List<String> added = new ArrayList<>();
+			
+			players.stream().map(username -> Ladder.getInstance().getOfflinePlayer(username)).filter(p -> p.hasPlayed()).forEach(player -> {
+				JsonObject hub = getAndAdd(player.getData(), "game", "others", "hub");
+
+				JsonArray array = hub.has("chests") ? hub.get("chests").getAsJsonArray() : new JsonArray();
+				array.add( Ladder.getInstance().getGson().toJsonTree( new CustomChest(id, false) ));
+				hub.add("chests", array);
+
+				added.add(player.getName());
+				
+				if(player instanceof Player){
+					Player p = (Player) player;
+					p.sendToBukkit("game");
+				}
+			});
+
+			new SlackMessage("Ajout d'une chest " + id + " à " + StringUtils.join(added, ", ") + " par " + sender.getName(), "BottyChest", false);
+		}
+	}
+
+	private JsonObject getAndAdd(JsonObject object, String... keys){
+		for(String key : keys){
+			if(object.has(key))
+				object = object.get(key).getAsJsonObject();
+			else{
+				JsonObject res = new JsonObject();
+			
+				object.add(key, res);
+				object = res;
+			}
+		}
+		
+		return object;
+	}
+
+	private void help(CommandSender sender){
+		sender.sendMessage(ChatColor.RED + "Utilisation n°1 : /givekey <id> <player1> <player2> ...");
+		sender.sendMessage(ChatColor.RED + "Utilisation n°2 : /givekey <id> -l <url>");
+	}
+
+	@Data
+	public class CustomChest {
+		public UUID			uuid;
+		public int 	   		typeId;
+		public boolean 		opened;
+
+		public CustomChest(int typeId, boolean opened) {
+			this.setUuid(UUID.randomUUID());
+			this.setTypeId(typeId);
+			this.setOpened(opened);
+		}
+	}
+}
