57703b8db94560f8324c6028ba4f64179a36235a
Asus <Asus@DESKTOP-LLJFP22>
2016-12-15 23:46:46 +0100

    fix

--- src/fr/badblock/sentry/SEntry.java
+++ src/fr/badblock/sentry/SEntry.java
@@ -7,10 +7,12 @@ import lombok.Setter;
 
 	private String		playerName;
 	private String		serverName;
+	private boolean		newGame;
 
-	public SEntry (String playerName, String serverName) {
+	public SEntry (String playerName, String serverName, boolean newGame) {
 		this.setPlayerName(playerName);
 		this.setServerName(serverName);
+		this.setNewGame(newGame);
 	}
 
 }
