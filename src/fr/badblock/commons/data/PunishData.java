fb1227111b9e6135c248b053bcd2bb9e32e6764c
xMalware <xmalware2@gmail.com>
2016-12-28 13:50:50 +0100

    a

--- src/fr/badblock/commons/data/PunishData.java
+++ src/fr/badblock/commons/data/PunishData.java
@@ -1,8 +1,16 @@
 package fr.badblock.commons.data;
 
-import lombok.Data;
+import java.io.Serializable;
 
-@Data public class PunishData {
+import com.mongodb.BasicDBObject;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data@EqualsAndHashCode(callSuper=false) public class PunishData extends BasicDBObject implements Serializable {
+	
+	private static final long serialVersionUID = -3763404142845364760L;
+
 	private boolean ban,
 					mute;
 	
