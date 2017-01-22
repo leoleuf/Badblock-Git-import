44552b578725ee2b8e989bce5567e0f13bfcd2ea
LeLanN <audrandoublet@gmail.com>
2017-01-22 21:09:35 +0100

    Adding C# files useful to both server and client

---
+++ Common_Is_NaN/Networking/InPacketHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Server_Is_NaN.Networking
+{
+    public abstract class InPacketHandler
+    {
+        public abstract void handleHandshake(In.Handshake packet);
+
+        public abstract void handleKeepAlive(In.KeepAlive packet);
+    }
+}
