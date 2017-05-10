386ef91c99eb8a96d3a28485c56450f53245fc10
xMalware <xmalware2@gmail.com>
2017-05-10 09:46:12 +0200

    fix

--- src/net/md_5/bungee/online/Utils.java
+++ src/net/md_5/bungee/online/Utils.java
@@ -13,13 +13,20 @@ public class Utils
   {
     try
     {
-      HttpsURLConnection connection = (HttpsURLConnection)new URL("https://sessionserver.mojang.com").openConnection();
-      connection.setConnectTimeout(10000);
-      connection.setReadTimeout(10000);
-      connection.setRequestMethod("HEAD");
-      int responseCode = connection.getResponseCode();
-      connection.disconnect();
-      return (200 <= responseCode) && (responseCode <= 399);
+        HttpsURLConnection connection = (HttpsURLConnection)new URL("https://sessionserver.mojang.com").openConnection();
+        connection.setConnectTimeout(10000);
+        connection.setReadTimeout(10000);
+        connection.setRequestMethod("HEAD");
+        int responseCode = connection.getResponseCode();
+        connection.disconnect();
+        HttpsURLConnection connection2 = (HttpsURLConnection)new URL("https://authserver.mojang.com").openConnection();
+        connection2.setConnectTimeout(10000);
+        connection2.setReadTimeout(10000);
+        connection2.setRequestMethod("HEAD");
+        int responseCode2 = connection2.getResponseCode();
+        connection2.disconnect();
+      
+      return (200 <= responseCode) && (responseCode <= 399) && (200 <= responseCode2) && (responseCode2 <= 399);
     }
     catch (IOException exception) {}
     return false;
