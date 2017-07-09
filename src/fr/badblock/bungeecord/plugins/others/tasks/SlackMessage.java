c264564e22bd1e278edb1de74e99ec262c8cd38d
xMalware <xmalware2@gmail.com>
2017-07-09 23:41:37 +0200

    fix

--- src/fr/badblock/bungeecord/plugins/others/tasks/SlackMessage.java
+++ src/fr/badblock/bungeecord/plugins/others/tasks/SlackMessage.java
@@ -5,7 +5,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-import org.json.simple.JSONObject;
+import fr.badblock.bungeecord.plugins.others.json.JSONObject;
 
 public class SlackMessage
 {
