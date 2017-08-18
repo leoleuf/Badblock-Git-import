fb2502091cbbbef305f9ff536de9730bd1a7aa53
Mathieu <mathieu.richard31@orange.fr>
2017-08-18 23:24:44 +0200

    Route Stats + Controller

---
+++ App/Controllers/StatsController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mathieu
+ * Date: 18/08/2017
+ * Time: 23:04
+ */
+
+namespace App\Controllers;
+
+
+class StatsController extends Controller
+{
+
+    public function home(){
+        //
+    }
+
+
+}
--- App/routes/web.php
+++ App/routes/web.php
@@ -7,5 +7,7 @@ $app->get('/', \App\Controllers\PagesController::class . ':home')->setName('home
 $app->get('/articles[/{p}]', \App\Controllers\BlogController::class . ':getAllPosts')->setName('all-posts');
 $app->get('/article/{slug}/{uuid}', \App\Controllers\BlogController::class . ':getPost')->setName('single-post');
 
+$app->get('/stats/{game}/{page}', \App\Controllers\StatsController::class . ':base')->setName('stats-home');
+
 
 
