61346abfacd1cf98e60665091f0ec494330780b8
Matthieu Bessat <mail@matthieubessat.fr>
2017-08-19 18:24:00 +0200

    Bug fix config/app.php forum url key

--- App/config/app.php
+++ App/config/app.php
@@ -2,6 +2,7 @@
 return [
 	'log' => getenv('LOG_ENABLED', true),
 	'twig' => [
-		'cache' => getenv('TWIG_CACHE', '../tmp/cache')
-	]
+		'cache' => getenv('TWIG_CACHE')
+	],
+	'forum_url' => getenv('FORUM_URL')
 ];
