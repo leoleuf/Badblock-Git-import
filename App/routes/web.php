7e9e14fa800b6268f36ed202319df4048cf10df6
Mathieu <mathieu.richard31@orange.fr>
2017-08-19 17:03:16 +0200

    Home page

--- App/routes/web.php
+++ App/routes/web.php
@@ -8,6 +8,7 @@ $app->get('/articles[/{p}]', \App\Controllers\BlogController::class . ':getAllPo
 $app->get('/article/{slug}/{uuid}', \App\Controllers\BlogController::class . ':getPost')->setName('single-post');
 
 $app->get('/stats/', \App\Controllers\StatsController::class . ':home')->setName('stats-home');
+$app->get('/stats/{game}/', \App\Controllers\StatsController::class . ':game')->setName('stats-game-np');
 $app->get('/stats/{game}/{page}', \App\Controllers\StatsController::class . ':game')->setName('stats-game');
 
 
