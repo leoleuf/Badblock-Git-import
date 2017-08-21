6775f5ede0c3a3e3da18d74927c1461643d5b073
Matthieu Bessat <mail@matthieubessat.fr>
2017-08-21 21:12:01 +0200

    Correction pull.sh

--- pull.sh
+++ pull.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #Automatic pull webhooks
 cd /home/web/dev-web/badblock-website/
-git pull https://git remote edit origin http://vps446463.ovh.net/Website/badblock-website.git
+git pull https://web_bot:!z6Nx-T5yRjfj6gf(BTtpmn*qbbxzv<d@http://vps446463.ovh.net/Website/badblock-website.git
 
 #update composer dependencies
 php composer.phar update
