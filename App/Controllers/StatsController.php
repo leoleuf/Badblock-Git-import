227e6a6f9452161299217653ade62b80f98e75e8
Matthieu Bessat <mail@matthieubessat.fr>
2017-08-21 22:24:08 +0200

    Stats controller

--- App/Controllers/StatsController.php
+++ App/Controllers/StatsController.php
@@ -14,57 +14,57 @@ use Psr\Http\Message\ResponseInterface;
 class StatsController extends Controller
 {
 
-    public function home(RequestInterface $request, ResponseInterface $response){
-        $this->container->view->render($response, 'pages/home-stats.twig');
-    }
+	public function home(RequestInterface $request, ResponseInterface $response){
+		$this->container->view->render($response, 'pages/home-stats.twig');
+	}
 
-    public function game(RequestInterface $request, ResponseInterface $response,$game,$page = null){
-        //Régulation vers fonction
-        if(!isset($page)){
-          $page = 1;
-        }
-        $game = $game['game'];
-        //Tower
-        if($game == "tower"){
-            $this->tower($page);
-        //Rush
-        }elseif ($game == "rush"){
-            $this->rush($page);
-        //Capture the flags
-        }elseif($game == "cts"){
-            $this->cts($page);
-        //Survival Games Solo
-        }elseif ($game == "SurvivalGamesSolo"){
-            $this->sgs($page);
-        //Survival Games Team
-        }elseif ($game == "SurvivalGamesTeams"){
-            $this->sgt($page);
-        //Pvp-Box
-        }elseif ($game == "pvpbox"){
-            $this->pvpbox($page);
-        //Faction
-        }elseif ($game == "Faction"){
-            $this->faction($page);
-        //UHC Solo
-        }elseif ($game == "uhcsolo"){
-            $this->uhcsolo($page);
-        //UHC Team
-        }elseif ($game == "uhcteam"){
-            $this->uhcteam($page);
-        //Space Balls
-        }elseif ($game == "SpaceBalls"){
-            $this->spaceball($page);
-        //DayZ
-        }elseif ($game == "dayz"){
-            $this->dayz($page);
-        }else{
-            //Erreur 404
-            return $response->withStatus(404);
-        }
-    }
+	public function game(RequestInterface $request, ResponseInterface $response,$game,$page = null){
+		//Régulation vers fonction
+		if(!isset($page)){
+			$page = 1;
+		}
+		$game = $game['game'];
+		//Tower
+		if($game == "tower"){
+			$this->tower($page);
+			//Rush
+		}elseif ($game == "rush"){
+			$this->rush($page);
+			//Capture the flags
+		}elseif($game == "cts"){
+			$this->cts($page);
+			//Survival Games Solo
+		}elseif ($game == "SurvivalGamesSolo"){
+			$this->sgs($page);
+			//Survival Games Team
+		}elseif ($game == "SurvivalGamesTeams"){
+			$this->sgt($page);
+			//Pvp-Box
+		}elseif ($game == "pvpbox"){
+			$this->pvpbox($page);
+			//Faction
+		}elseif ($game == "Faction"){
+			$this->faction($page);
+			//UHC Solo
+		}elseif ($game == "uhcsolo"){
+			$this->uhcsolo($page);
+			//UHC Team
+		}elseif ($game == "uhcteam"){
+			$this->uhcteam($page);
+			//Space Balls
+		}elseif ($game == "SpaceBalls"){
+			$this->spaceball($page);
+			//DayZ
+		}elseif ($game == "dayz"){
+			$this->dayz($page);
+		}else{
+			//Erreur 404
+			return $response->withStatus(404);
+		}
+	}
 
 
-    public function tower($page){
+	public function tower($page){
 //        $m = new $this->mongo;
 //        $c = $m->selectCollection("phpt", "find");
 //
@@ -83,12 +83,13 @@ class StatsController extends Controller
 
 		var_dump($insertOneResult->getInsertedId());*/
 
-		$collection = $this->mongo->test->users;
+		$collection = $this->mongo->test->test;
 
-		$document = $collection->find(['_id' => '5999e7c8b6182c29a83df7c6']);
+		$cursor = $collection->findOne(['uniqueId' => 'f77952e6-1fa9-3336-a60e-1d47e340d71a']);
 
-		var_dump($document);
-    }
+
+		var_dump($cursor->game->stats->tower);
+	}
 
 
 }
