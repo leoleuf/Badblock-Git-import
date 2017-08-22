a4701aa6079c8b3f00610b87013952a005bab72f
xMalware <xmalware2@gmail.com>
2017-08-22 21:48:27 +0200

    f

--- src/fr/badblock/rabbitconnector/RabbitListenerType.java
+++ src/fr/badblock/rabbitconnector/RabbitListenerType.java
@@ -4,5 +4,11 @@ public enum RabbitListenerType {
 
 	MESSAGE_BROKER,
 	SUBSCRIBER;
+
+	public static RabbitListenerType get(String name) {
+		for (RabbitListenerType type : values())
+			if (type.name().equalsIgnoreCase(name)) return type;
+		return null;
+	}
 	
 }
