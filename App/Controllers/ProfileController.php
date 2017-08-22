41d47bd6088af49a52bc59263a68348bfc3d8be2
Mathieu <mathieu.richard31@orange.fr>
2017-08-22 19:28:38 +0200

    Algo Point mini-jeux

--- App/Controllers/ProfileController.php
+++ App/Controllers/ProfileController.php
@@ -28,8 +28,150 @@ class ProfileController extends Controller
         }
 
 
-        var_dump($cursor->game->stats);
+        var_dump(ProfileController::tower($cursor->game->stats->tower));
+        var_dump(ProfileController::spaceball($cursor->game->stats->spaceballs));
+        var_dump(ProfileController::rushs($cursor->game->stats->rush));
+        var_dump(ProfileController::pearlswar($cursor->game->stats->pearlswar));
+        var_dump(ProfileController::uhcspeed($cursor->game->stats->uhcspeed));
 
+    }
+
+
+
+    public function tower($var){
+
+        //Algo celon Xmalware
+
+        $c1 = $var['marks']+$var['wins'];
+        //pas de division par zéro
+        if ($var['deaths'] > 0){
+            $c2 = $var['kills']/$var['deaths'];
+        }else{
+            $c2 = $var['kills'];
+        }
+        $c3 = $c1*$c2;
+        //pas de division par zéro
+        if ($var['looses'] > 0){
+            $c4 = $c3 /$var['looses'];
+        }else{
+            $c4 = $c3;
+        }
+
+        //Arrondis
+
+        $c4 = round($c4,0,PHP_ROUND_HALF_UP);
+
+        return $c4;
+
+    }
+
+    public function spaceball($var){
+
+        //Algo celon Xmalware
+
+        $c1 = $var['diamonds']+$var['wins'];
+        //pas de division par zéro
+        if ($var['deaths'] > 0){
+            $c2 = $var['kills']/$var['deaths'];
+        }else{
+            $c2 = $var['kills'];
+        }
+        $c3 = $c1*$c2;
+        //pas de division par zéro
+        if ($var['looses'] > 0){
+            $c4 = $c3 /$var['looses'];
+        }else{
+            $c4 = $c3;
+        }
+
+        //Arrondis
+
+        $c4 = round($c4,0,PHP_ROUND_HALF_UP);
+
+        return $c4;
+
+    }
+
+    public function rushs($var){
+
+        //Algo celon Xmalware
+
+        $c1 = $var['brokenbeds']+$var['wins'];
+        //pas de division par zéro
+        if ($var['deaths'] > 0){
+            $c2 = $var['kills']/$var['deaths'];
+        }else{
+            $c2 = $var['kills'];
+        }
+        $c3 = $c1*$c2;
+        //pas de division par zéro
+        if ($var['looses'] > 0){
+            $c4 = $c3 /$var['looses'];
+        }else{
+            $c4 = $c3;
+        }
+
+        //Arrondis
+
+        $c4 = round($c4,0,PHP_ROUND_HALF_UP);
+
+        return $c4;
+
+    }
+
+
+    public function pearlswar($var){
+
+        //Algo celon Xmalware
+
+        $c1 = $var['wins']*1.5;
+        //pas de division par zéro
+        if ($var['deaths'] > 0){
+            $c2 = $var['kills']/$var['deaths'];
+        }else{
+            $c2 = $var['kills'];
+        }
+        $c3 = $c1*$c2;
+        //pas de division par zéro
+        if ($var['looses'] > 0){
+            $c4 = $c3 /$var['looses'];
+        }else{
+            $c4 = $c3;
+        }
+
+        //Arrondis
+
+        $c4 = round($c4,0,PHP_ROUND_HALF_UP);
+
+        return $c4;
+
+    }
+
+
+    public function uhcspeed($var){
+
+        //Algo celon Xmalware
+
+        $c1 = $var['wins']*1.5;
+        //pas de division par zéro
+        if ($var['deaths'] > 0){
+            $c2 = $var['kills']/$var['deaths'];
+        }else{
+            $c2 = $var['kills'];
+        }
+        $c3 = $c1*$c2;
+        //pas de division par zéro
+        if ($var['looses'] > 0){
+            $c4 = $c3 /$var['looses'];
+        }else{
+            $c4 = $c3;
+        }
+
+        //Arrondis
+
+        $c4 = round($c4,0,PHP_ROUND_HALF_UP);
+
+        return $c4;
 
     }
 
