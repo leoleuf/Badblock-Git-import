d9505037959bec8160cd2d032d7b682335677b1b
Matthieu Bessat <mail@matthieubessat.fr>
2017-08-22 18:41:26 +0200

    fix pull.sh

--- pull.sh
+++ pull.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #Automatic pull webhooks
 cd /home/web/dev-web/badblock-website/
-git pull web_bot:tG5wJRLUzbTi3u0nVvpRRRSabP9vt36DxM6N@vps446463.ovh.net/Website/badblock-website.git
+git pull http://lefuturiste:A26EKAHJVKH8DGR3EDK2@vps446463.ovh.net/Website/badblock-website.git
 
