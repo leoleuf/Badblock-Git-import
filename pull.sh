aa2807335161ec4c5a0aeca9bdd2b3dcf11555b2
Matthieu Bessat <mail@matthieubessat.fr>
2017-08-22 11:13:23 +0200

    fix pull.sh bug (2)

--- pull.sh
+++ pull.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #Automatic pull webhooks
 cd /home/web/dev-web/badblock-website/
-git pull https://web_bot:9zppKeF5PR9xC6mbVnhK@http://vps446463.ovh.net/Website/badblock-website.git
+git pull https://web_bot:kPnZSY3DW9gCCnyBxrA2Fm6eAa3tcafe@http://vps446463.ovh.net/Website/badblock-website.git
 
 #update composer dependencies
 php composer.phar update
