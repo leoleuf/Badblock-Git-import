714c7f09e52f23ee19eeb1c528e3b82f53ad10f1
Mathieu <mathieu.richard31@orange.fr>
2017-08-25 13:05:34 +0200

    Page Profile

--- App/Controllers/ProfileController.php
+++ App/Controllers/ProfileController.php
@@ -26,9 +26,21 @@ class ProfileController extends Controller
             return $this->container['notFoundHandler']($request, $response);
         }
 
+
+        $user->game->stats->tower['cpoints'] = $this->tower($user->game->stats->tower);
+        $user->game->stats->rush['cpoints'] = $this->rushs($user->game->stats->rush);
+        $user->game->stats->survival['cpoints'] = $this->survival($user->game->stats->survival);
+        $user->game->stats->uhcspeed['cpoints'] = $this->uhcspeed($user->game->stats->uhcspeed);
+        $user->game->stats->pearlswar['cpoints'] = $this->pearlswar($user->game->stats->pearlswar);
+        $user->game->stats->spaceballs['cpoints'] = $this->spaceball($user->game->stats->spaceballs);
+        $user->game->stats->cts['cpoints'] = $this->cts($user->game->stats->cts);
+
+
+
         //return view
         return $this->render($response, 'user.profile', [
             'user' => $user]);
+
     }
 
 
@@ -146,6 +158,61 @@ class ProfileController extends Controller
 
     public function uhcspeed($var){
 
+    //Algo celon Xmalware
+
+    $c1 = $var['wins']*1.5;
+    //pas de division par zéro
+    if ($var['deaths'] > 0){
+        $c2 = $var['kills']/$var['deaths'];
+    }else{
+        $c2 = $var['kills'];
+    }
+    $c3 = $c1*$c2;
+    //pas de division par zéro
+    if ($var['looses'] > 0){
+        $c4 = $c3 /$var['looses'];
+    }else{
+        $c4 = $c3;
+    }
+
+    //Arrondis
+
+    $c4 = round($c4,0,PHP_ROUND_HALF_UP);
+
+    return $c4;
+
+}
+
+    public function cts($var){
+
+        //Algo celon Xmalware
+
+        $c1 = $var['capturedflags']+$var['wins'];
+        //pas de division par zéro
+        if ($var['deaths'] > 0){
+            $c2 = $var['kills']/$var['deaths'];
+        }else{
+            $c2 = $var['kills'];
+        }
+        $c3 = $c1*$c2;
+        //pas de division par zéro
+        if ($var['looses'] > 0){
+            $c4 = $c3 /$var['looses'];
+        }else{
+            $c4 = $c3;
+        }
+
+        //Arrondis
+
+        $c4 = round($c4,0,PHP_ROUND_HALF_UP);
+
+        return $c4;
+
+    }
+
+
+    public function survival($var){
+
         //Algo celon Xmalware
 
         $c1 = $var['wins']*1.5;
@@ -169,10 +236,6 @@ class ProfileController extends Controller
 
         return $c4;
 
-        //return view
-		return $this->render($response, 'user.profile', [
-			'user' => $user
-		]);
     }
 
 
