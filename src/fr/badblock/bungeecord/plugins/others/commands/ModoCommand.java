824aa93a69d7acfd519a5e108b491029973d9254
xMalware <xmalware2@gmail.com>
2017-08-26 21:28:42 +0200

    f

--- src/fr/badblock/bungeecord/plugins/others/commands/ModoCommand.java
+++ src/fr/badblock/bungeecord/plugins/others/commands/ModoCommand.java
@@ -22,21 +22,21 @@ public class ModoCommand extends Command {
 	@SuppressWarnings("deprecation")
 	public void execute(CommandSender sender, String[] args) {
 		if (args.length < 1) {
-			sender.sendMessage("§e------------------------------------------------------");
+			sender.sendMessage("§e--------------------------------------------------");
 			sender.sendMessage("§fPosez votre question à la modération !");
 			sender.sendMessage(" ");
-			sender.sendMessage("	§bUtilisation: §f/modo §b<message>");
+			sender.sendMessage("     §bUtilisation: §f/modo §b<message>");
 			sender.sendMessage(" ");
 			sender.sendMessage("§cTout abus de cette commande vaudra un bannissement.");
 			sender.sendMessage("§fN'oubliez pas d'activer vos MP pour avoir une réponse.");
 			sender.sendMessage("§cLes signalements ne se font pas par cette commande.");
-			sender.sendMessage("§e------------------------------------------------------");
+			sender.sendMessage("§e--------------------------------------------------");
 			return;
 		}
 		String playerName = sender.getName();
 		long timestamp = System.currentTimeMillis();
 		if (times.containsKey(playerName) && times.get(playerName) > timestamp) {
-			sender.sendMessage("§e------------------------------------------------------");
+			sender.sendMessage("§e--------------------------------------------------");
 			sender.sendMessage("§cVeuillez patienter entre chaque message.");
 			sender.sendMessage(" ");
 			sender.sendMessage("§fLa modération doit répondre à tout le serveur.");
@@ -45,7 +45,7 @@ public class ModoCommand extends Command {
 			sender.sendMessage("§bUn message est autorisé toutes les §cdeux minutes§b.");
 			sender.sendMessage("§cEn cas de réponse d'un modérateur, cliquez sur son DM");
 			sender.sendMessage("§cpour y répondre au lieu d'utiliser le /modo.");
-			sender.sendMessage("§e------------------------------------------------------");
+			sender.sendMessage("§e--------------------------------------------------");
 			return;
 		}
 		long expire = timestamp + time;
@@ -53,7 +53,7 @@ public class ModoCommand extends Command {
 		String message = StringUtils.join(args, " ");
 		BadBlockBungeeOthers.getInstance().getRabbitService().sendSyncPacket("badfilter", "§6[/modo] &f" + playerName + " >> §7" + message, Encodage.UTF8, RabbitPacketType.PUBLISHER, 5000, false);
 		BadBlockBungeeOthers.getInstance().getRabbitService().sendSyncPacket("badfilter", "§fRépondez à la question en faisant /msg " + playerName, Encodage.UTF8, RabbitPacketType.PUBLISHER, 5000, false);
-		sender.sendMessage("§e------------------------------------------------------");
+		sender.sendMessage("§e--------------------------------------------------");
 		sender.sendMessage("§aVotre question a été posée à la modération :");
 		sender.sendMessage(message);
 		sender.sendMessage(" ");
@@ -61,7 +61,7 @@ public class ModoCommand extends Command {
 		sender.sendMessage("§cTout abus de cette commande vaudra un bannissement.");
 		sender.sendMessage("§cLes signalements ne se font pas par cette commande.");
 		sender.sendMessage("§fN'oubliez pas d'activer vos MP pour avoir une réponse.");
-		sender.sendMessage("§e------------------------------------------------------");
+		sender.sendMessage("§e--------------------------------------------------");
 	}
 
 }
