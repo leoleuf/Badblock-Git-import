30b8b86a518ab2f05a30584e9eb0b0a55f0e2c05
Matthieu Bessat <mail@matthieubessat.fr>
2017-09-21 17:47:50 +0200

    Adding composer auto update

--- pull.php
+++ pull.php
@@ -1,5 +1,5 @@
 <?php
 chdir('/home/web/dev-web/badblock-website');
 shell_exec('git pull http://web_bot:kPnZSY3DW9gCCnyBxrA2Fm6eAa3tcafe@vps446463.ovh.net/Website/badblock-website.git');
-shell_exec('composer install');
-shell_exec('composer update');
+shell_exec('php composer.phar install');
+shell_exec('php composer.phar update');
