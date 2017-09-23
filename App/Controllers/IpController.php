b87117874c457819e8b4fdd57c23b2fb2ddc7826
Mathieu <mathieu.richard31@orange.fr>
2017-09-23 09:37:10 +0200

    IPV6

--- App/Controllers/IpController.php
+++ App/Controllers/IpController.php
@@ -10,15 +10,6 @@ class IpController extends Controller
 
 		    $addr = $_SERVER['REMOTE_ADDR'];
 
-	        //Variables
-			$result =  [];
-			$eu = array("FR", "ES", "PT", "GB", "IE", "BE", "NL", "LU", "DE", "IT", "DK", "SE", "CZ", "AT", "SI", "HR", "BA", "ME", "PL", "SK", "HU", "HR", "RS", "BG", "MK", "AL", "GR", "TR", "CY", "MT", "EE", "LV", "LT", "FI");
-			$na = array("US", "CA", "MX");
-
-			//Geo IP
-			$code = "FR";
-			$gi = geoip_open("../App/config/GeoIPv6.dat", GEOIP_STANDARD);
-
             $db = array(
                 'Geo4' => geoip_open("../App/config/geoip.dat", GEOIP_STANDARD),
                 'Geo6' => geoip_open("../App/config/GeoIPv6.dat", GEOIP_STANDARD),
@@ -31,27 +22,8 @@ class IpController extends Controller
                 $code = geoip_country_code_by_addr($db, $addr);
             }
 
-			geoip_close($gi);
+            var_dump($code->country_code);
 
-            var_dump($code);
-
-            //Check Pays
-			if (in_array($code, $eu)) {
-				$iptos = "eu.badblock.fr";
-			}elseif (in_array($code, $na)){
-				$iptos = "na.badblock.fr";
-			}else{
-				$iptos = "play.badblock.fr";
-			}
-
-			//Copille et envoie
-			array_push($result,$iptos,$code,$pays);
-
-			//Mise en cache
-
-
-
-        return json_encode($result);
 
 
 	}
