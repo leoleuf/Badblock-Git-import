4475f5720a9947de9f082754e0d602080896294e
Mathieu <mathieu.richard31@orange.fr>
2017-10-05 21:00:57 +0200

    Api Shop

--- App/Controllers/Api/ShopApiController.php
+++ App/Controllers/Api/ShopApiController.php
@@ -8,16 +8,31 @@
 
 namespace App\Controllers\Api;
 
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 
 class ShopApiController extends \App\Controllers\Controller
 {
 
-    public function getCreateCacheShop(RequestInterface $request, ResponseInterface $response){
+    public function getCreateCacheShopList(RequestInterface $request, ResponseInterface $response){
 
+        //Check MongoDB
+        $cursor = $this->mongo->dev->server->findOne(['name' => "SkyBlock"]);
+        //Array
+        $listserver = [];
+        //Lecture + push
+        foreach ($cursor as $df) {
+            array_push($listserver,array($df->id,$df->name));
+        }
+        //Mise en Cache
 
+        $this->redis->setJson('shop.listsrv');
 
-
-
+        //Renvoie d'un code de succès
+        return $response->write('Success writing shop cache')->withStatus(200);
+        $this->log->info('"StaffApiController\getCreateCacheAllStaff": Success writing shop cache');
 
     }
 
