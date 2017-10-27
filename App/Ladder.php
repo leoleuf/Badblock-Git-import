f65e1347c907fab90a1f2a54c7ffc92820b2147a
Matthieu Bessat <mail@matthieubessat.fr>
2017-10-27 10:59:37 +0200

    BIG Refactoring of Ladder SDK class

--- App/Ladder.php
+++ App/Ladder.php
@@ -8,74 +8,148 @@
 
 namespace App;
 
-
+/**
+ * Class Ladder
+ *
+ * SDK for internal minecraft bungeecoord HTTP REST api
+ *
+ * @package App
+ */
 class Ladder
 {
-    public function __construct($container, $config)
-    {
-        $this->container = $container;
-        $this->guzzle = $container->guzzle;
+	private $scheme = 'http://';
 
-        $this->config = $config;
-    }
+	public function __construct($container, $config)
+	{
+		$this->container = $container;
+		$this->guzzle = $container->guzzle;
+		$this->config = $config;
+	}
 
+	/**
+	 * Send data to a url without endpoint
+	 *
+	 * @param $type
+	 * @param $data
+	 * @return mixed
+	 */
+	private function sendData($type, $data)
+	{
+		$request = $this->guzzle->request(
+			'POST',
+			$this->scheme . $this->config->ip . ':' . $this['port'] . "/" . $type,
+			[
+				'form_params' => $data
+			]
+		);
+		$response = $request->getBody();
 
+		return $response;
+	}
 
-    // Do not use
-    function sendData($type, $data) {
-        $curl = curl_init("http://". $this->config['ip'].":". $this->config['port']."/".$type);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return $response;
-    }
+	/**
+	 * Send a broadcast message to defined server
+	 *
+	 * @param $server
+	 * @param $message
+	 * @return mixed
+	 */
+	public function serverBroadcast($server, $message)
+	{
+		return $this->sendData("server/broadcast/", ["server" => $server, "message" => $message]);
+	}
 
-    // Broadcast to a server
-    function serverBroadcast($server, $message) {
-        return $this->sendData("server/broadcast/", array("server" => $server, "message" => $message));
-    }
+	/**
+	 * Know if player exist in database of network
+	 *
+	 * @param $player
+	 * @return mixed
+	 */
+	public function playerExists($player)
+	{
+		return json_encode($this->sendData("players/exists/", ["name" => $player]), true)->exist;
+	}
 
-    // If player exists
-    function playerExists($player) {
-        return json_encode($this->sendData("players/exists/", array("name" => $player)), true)->exist;
-    }
+	/**
+	 * Know if the defined player is online
+	 *
+	 * @param $player
+	 * @return mixed
+	 */
+	public function playerOnline($player)
+	{
+		return json_decode($this->sendData("players/isConnected/", ["name" => $player]), true);
+	}
 
-    // If player is online
-    function playerOnline($player) {
-        return json_decode($this->sendData("players/isConnected/", array("name" => $player)), true);
-    }
+	/**
+	 * Get player data
+	 *
+	 * @param $player
+	 * @return string
+	 */
+	public function playerGetData($player)
+	{
+		return json_encode($this->sendData("players/getData/", ["name" => $player]), true);
+	}
 
-    // Get player data
-    function playerGetData($player) {
-        return json_encode($this->sendData("players/getData/", array("name" => $player)), true);
-    }
+	/**
+	 * unknow
+	 * //TODO: make doc
+	 *
+	 * @param $player
+	 * @return mixed
+	 */
+	public function playerGetConnectedServer($player)
+	{
+		return json_decode($this->sendData("players/getConnectedServer/", ["name" => $player]));
+	}
 
-    // Get player server
-    function playerGetConnectedServer($player) {
-        return json_decode($this->sendData("players/getConnectedServer/", array("name" => $player)));
-    }
+	/**
+	 * Send private message to a specified online player in the network
+	 *
+	 * @param $player
+	 * @param $message
+	 * @return mixed
+	 */
+	public function playerSendMessage($player, $message)
+	{
+		return $this->sendData("players/sendMessage/", ["name" => $player, "message" => $message]);
+	}
 
-    // Player send message
-    function playerSendMessage($player, $message) {
-        return $this->sendData("players/sendMessage/", array("name" => $player, "message" => $message));
-    }
+	/**
+	 * unknow
+	 * //TODO: make doc
+	 *
+	 * @param $player
+	 * @return mixed
+	 */
+	public function playerSendServerMessage($player, $message)
+	{
+		return $this->sendData("players/sendServerMessage/", ["name" => $player, "message" => $message]);
+	}
 
-    // Player send server message
-    function playerSendServerMessage($player, $message) {
-        return $this->sendData("players/sendServerMessage/", array("name" => $player, "message" => $message));
-    }
+	/**
+	 * Unknow
+	 * Update player data
+	 *
+	 * @param $player
+	 * @return mixed
+	 */
+	public function playerUpdateData($player, $dataName, $dataData)
+	{
+		return $this->sendData("players/updateData/", ["name" => $player, $dataName => $dataData]);
+	}
 
-    // Player update data
-    function playerUpdateData($player, $dataName, $dataData) {
-        return $this->sendData("players/updateData/", array("name" => $player, $dataName => $dataData));
-    }
-
-    // Player update data
-    function playerAddGroup($player, $group, $duration = -1) {
-        return $this->sendData("players/addGroup/", array("name" => $player, "group" => $group, "duration" => $duration));
-    }
+	/**
+	 * Add player in a group
+	 *
+	 * @param $player
+	 * @return mixed
+	 */
+	public function playerAddGroup($player, $group, $duration = -1)
+	{
+		return $this->sendData("players/addGroup/", ["name" => $player, "group" => $group, "duration" => $duration]);
+	}
 
 
 }
