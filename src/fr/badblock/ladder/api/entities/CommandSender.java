70eb70a79bafd7a6e364673d490381fd71fc590a
xMalware <xmalware2@gmail.com>
2017-12-03 00:27:04 +0100

    'ok'

--- src/fr/badblock/ladder/api/entities/CommandSender.java
+++ src/fr/badblock/ladder/api/entities/CommandSender.java
@@ -1,13 +1,14 @@
 package fr.badblock.ladder.api.entities;
 
-public interface CommandSender {
-	public String getName();
-	
-	public boolean hasPermission(String permission);
-	
-	public void sendMessage(String message);
-	
-	public void sendMessages(String... messages);
-	
-	public void forceCommand(String... commands);
+public abstract interface CommandSender
+{
+  public abstract String getName();
+  
+  public abstract boolean hasPermission(String paramString);
+  
+  public abstract void sendMessage(String paramString);
+  
+  public abstract void sendMessages(String... paramVarArgs);
+  
+  public abstract void forceCommand(String... paramVarArgs);
 }
