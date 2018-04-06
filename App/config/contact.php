1d1d490860330976713ce0608bd1ce837373aa96
mmaniaque <rondeaum4@gmail.com>
2018-04-06 17:00:00 -0400

    add discord url contact.php

--- App/config/contact.php
+++ App/config/contact.php
@@ -1,8 +1,9 @@
 <?php
 return [
 	'contact' => [
-		'twitter' => 'http://twitter.com/BadBlockGame',
-		'facebook' => 'https://www.facebook.com/BadBlockGame/',
+		'twitter' => 'https://twitter.com/BadBlockGame',
+		'facebook' => 'https://www.facebook.com/BadBlockGame',
 		'youtube' => 'https://www.youtube.com/channel/UCQGBfKWLa88_pQVHdCJbuIw',
+        'discord' => 'https://badblock.fr/discord',
 	]
 ];
