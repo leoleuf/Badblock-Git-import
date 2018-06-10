ae21fd1290d77ba619110642b2f73c2f505abd0a
Skript <mathieu.richard31@orange.fr>
2018-06-10 22:20:27 +0200

    bbbb

--- App/Controllers/Api/StatsApiController.php
+++ App/Controllers/Api/StatsApiController.php
@@ -62,7 +62,7 @@ class StatsApiController extends \App\Controllers\Controller
         $msg_forum = $this->container->mysql_forum->fetchRow('SELECT COUNT(*) FROM xf_post;')["COUNT(*)"];
 
         //Connecté sur TS
-        $ts_connected = $this->teamspeak->online();
+        //$ts_connected = $this->teamspeak->online();
 
         //nombre d'articles
         $article = $this->redis->get('posts_count');
--- pull.php
+++ pull.php
@@ -1,18 +1,5 @@
 <?php
 chdir('/home/web/dev-web/badblock-website');
-shell_exec('git pull http://web_bot:kPnZSY3DW9gCCnyBxrA2Fm6eAa3tcafe@lusitania.badblock.fr/Website/badblock-website.git');
+shell_exec('git pull http://websyncbot:JTXg4hc7LxMUc8ERsjEC4GhUMMWCRhBWSeALBq6FjxbakM5Vwvzv3dzJgFm32mS7@lusitania.badblock.fr/Website/badblock-website.git');
 shell_exec('php composer.phar install');
 shell_exec('php composer.phar update');
-
-$data = array("username" => "Logger Site","embeds" => array(0 => array(
-            "url" => "https://dev-web.badblock.fr",
-            "title" => "GitLab Puller ",
-            "description" => "Site Web de démo update !",
-            "color" => 65280
-        )));
-		
-		        $curl = curl_init("https://discordapp.com/api/webhooks/373808432324542464/g_ZJQXYA0yPj7LyHebSQZA14eAbLxB7w8idL50weFHX-rSGpdI-cu-fiu0gbHl9BIa8F");
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_exec($curl);
