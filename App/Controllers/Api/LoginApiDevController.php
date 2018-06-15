bc4b87ef2b49bdbda4536b33192debd4fa3ec2ab
Skript <mathieu.richard31@orange.fr>
2018-06-15 21:42:47 +0200

    b

--- App/Controllers/Api/LoginApiDevController.php
+++ App/Controllers/Api/LoginApiDevController.php
@@ -10,27 +10,31 @@ class LoginApiDevController extends \App\Controllers\Controller
 
     public function login(ServerRequestInterface $request, ResponseInterface $response,$args){
 
-                //user
-        $user = $this->xenforo->getUser($args['username']);
+         if ($this->container->config['app_debug'] == 1){
+             //user
+             $user = $this->xenforo->getUser($args['username']);
 
-        $user['secondary_group_ids'] = explode(",", $user['secondary_group_ids']);
+             $user['secondary_group_ids'] = explode(",", $user['secondary_group_ids']);
 
-        //mise de l'utilisateur en session
-        $this->session->set('user', [
-            'id' => $user['user_id'],
-            'username' => $user['username'],
-            'email' => $user['email'],
-            'user_group_id' => $user['user_group_id'],
-            'secondary_group_ids' => [],
-            'custom_title' => $user['custom_title'],
-            'is_admin' => $user['is_admin'],
-            'is_banned' => $user['is_banned'],
-            'is_staff' => $user['is_staff'],
-            'is_moderator' => $user['is_moderator']
-        ]);
+             //mise de l'utilisateur en session
+             $this->session->set('user', [
+                 'id' => $user['user_id'],
+                 'username' => $user['username'],
+                 'email' => $user['email'],
+                 'user_group_id' => $user['user_group_id'],
+                 'secondary_group_ids' => [],
+                 'custom_title' => $user['custom_title'],
+                 'is_admin' => $user['is_admin'],
+                 'is_banned' => $user['is_banned'],
+                 'is_staff' => $user['is_staff'],
+                 'is_moderator' => $user['is_moderator']
+             ]);
 
 
-        return $response->write('Ok login')->withStatus(200);
+             return $response->write('Ok login')->withStatus(200);
+         }else{
+             return $response->write('Nope')->withStatus(200);
+         }
 
 
     }
