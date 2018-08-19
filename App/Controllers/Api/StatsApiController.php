346b3853572592caa6a1760125c5e613b7fc47bc
Mathieu <mathieu.richard31@orange.fr>
2018-08-19 22:14:38 +0200

    some bug

--- App/Controllers/Api/StatsApiController.php
+++ App/Controllers/Api/StatsApiController.php
@@ -23,9 +23,7 @@ class StatsApiController extends \App\Controllers\Controller
 
         //Lecture du classement
         $query = "show tables;";
-
-        $re = [];
-
+        
         foreach ($this->container->mysql_rankeds->fetchRowManyCursor($query) as $game)
         {
             $name = $game["Tables_in_rankeds"];
