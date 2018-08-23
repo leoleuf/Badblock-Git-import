2fadec2ab24c06ea8cb584ce8507a09a3da54c47
xMalware <xmalware2@gmail.com>
2018-08-23 03:46:16 +0200

    ok

--- App/Paypal.php
+++ App/Paypal.php
@@ -69,7 +69,9 @@ class Paypal {
         $response = curl_exec($curl);
         $responseArray = array();
         parse_str($response, $responseArray);
-        //preint_r($responseArray);
+        var_dump($responseArray);
+        exit;
+        return false;
 
         if(curl_errno($curl)){
             $this->errors = curl_error($curl);
