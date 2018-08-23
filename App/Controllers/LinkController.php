9507d5fee6b3252573ec104516e7b497d788c41d
Mathieu <mathieu.richard31@orange.fr>
2018-08-23 10:00:15 +0200

    fix linkage

--- App/Controllers/LinkController.php
+++ App/Controllers/LinkController.php
@@ -42,16 +42,8 @@ class LinkController extends Controller
                             return $this->redirect($response, $_SERVER['HTTP_REFERER']);
                         }else{
                             //Création du code random
-                            $chars = "FLUORLEBG";
-                            srand((double)microtime()*1000000);
-                            $i = 0;
-                            $pass = '' ;
-                            while ($i <= 6) {
-                                $num = rand() % 33;
-                                $tmp = substr($chars, $num, 1);
-                                $pass = $pass . $tmp;
-                                $i++;
-                            }
+                            $pass = $this->generateRandomString();
+
                             //Set code in Redis cache
                             $this->redis->set('link:'.$username,$pass);
                             $this->redis->expire('link:'.$username, 3600);
@@ -101,68 +93,16 @@ class LinkController extends Controller
 
 
 
-    public function getLink(RequestInterface $request, ResponseInterface $response){
-        //On vérifie si le joueur est connecté
-        return $this->render($response, 'user.link');
-    }
-
-    public function Link(RequestInterface $request, ResponseInterface $response){
-
-        if ($_POST['etape'] == 1){
-            if (!isset($_POST['link'])){
-                return $response->write("Invalide Inputss")->withStatus(500);
-            }
-
-            $collection = $this->mongo->dat_users;
-            $user = $collection->findOne(['realName' => $_POST['link']]);
-
-            //On vérifie si le joueur existe sur le serveur
-            if ($user != null){
-                //on vérif si le joueur est pas déjà link
-                if ($user["website"]['link'] == false){
-                    //On vérifie si le joueur est connecté
-                    if ($this->ladder->playerOnline($_POST['link'])['connected'] == true){
-                        //Création du code random
-                        $chars = "AZERTYUI3456789OPQSDFGHJKLMWXCVBN12#";
-                        srand((double)microtime()*1000000);
-                        $i = 0;
-                        $pass = '' ;
-
-                        while ($i <= 6) {
-                            $num = rand() % 33;
-                            $tmp = substr($chars, $num, 1);
-                            $pass = $pass . $tmp;
-                            $i++;
-                        }
-                        //Set code in Redis cache
-                        $this->redis->setJson('link:'.$_POST['link'],$pass);
-                        $this->ladder->playerSendMessage($_POST['link'],"administarteur");
-
-                        return $response->write("ok")->withStatus(200);
-
-                    }else{
-                        return $response->write("Non connecté au serveur")->withStatus(404);
-                    }
-                }else{
-                    return $response->write("Compte déjà link")->withStatus(400);
-                }
-            }else{
-                return $response->write("Compte inconnu")->withStatus(500);
-            }
-        }else{
-            //Renvoie du code de linkage
-            if ($_POST["code"] == $this->redis->getJson('link:'.$_POST['user'])){
-                return $response->write("ok")->withStatus(200);
-            }else{
-                $this->flash->addMessage('link_error', "Mauvais code !");
-
-                //redirect to last page
-                return $this->redirect($response, $_SERVER['HTTP_REFERER']);
-            }
+    public function generateRandomString($length = 10)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++)
+        {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
         }
-
-
-
+        return $randomString;
     }
 
 }
