328446d95c56f219bb3fb5c54edcd901808b3841
xMalware <xmalware2@gmail.com>
2018-08-23 04:06:07 +0200

    ok

--- App/Controllers/DedipassController.php
+++ App/Controllers/DedipassController.php
@@ -27,6 +27,9 @@ class DedipassController extends Controller
         else {
           $dedipass = file_get_contents('http://api.dedipass.com/v1/pay/?public_key='.getenv("DEDIPASS_PUBLIC_KEY")
               .'&private_key='.getenv("DEDIPASS_PRIVATE_KEY").'&code=' . $code);
+          var_dump(getenv("DEDIPASS_PUBLIC_KEY"));
+          exit;
+          return;
           if($dedipass->status == 'success') { 
               $virtual_currency = $dedipass->virtual_currency;
               // Détection d'une quelconque action
