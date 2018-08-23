48321cedc5b52b53a5695523c6e9bd0a6def67cc
xMalware <xmalware2@gmail.com>
2018-08-23 23:07:21 +0200

    SEO

--- App/Controllers/StarpassController.php
+++ App/Controllers/StarpassController.php
@@ -99,7 +99,7 @@ class StarpassController extends Controller
 
         $dat = [$date,$datas, $pays, $palier, $id_palier, $type];
         $insertedId = $this->container->mongo->funds_logs->insertOne($dat);
-        $insertedId = $insertedId;
+        $insertedId = $insertedId->insertedId->oid;
         var_dump($insertedId);
         exit;
         return;
