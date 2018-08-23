9f42139776b781178734c631b919e513988fb922
Quentin B <gastbob40@gmail.com>
2018-08-23 13:53:39 +0200

    ajout de mongo starpass

--- App/Controllers/StarpassController.php
+++ App/Controllers/StarpassController.php
@@ -25,6 +25,45 @@ class StarpassController extends Controller
         $PALIER = $_GET['PALIER'];
         $ID_PALIER = $_GET['ID_PALIER'];
         $TYPE = $_GET['TYPE'];
+        $obj = (object) array('1' => '50');
+
+        $virtual_currency = $obj("1");
+        // Sauvegarde dans mongoDB
+        $starpass->date = date('Y-m-d H:i:s');
+        $this->container->mongo->funds_logs->insertOne($starpass);
+
+                $user = $this->container->mongoServer->players->findOne(['name' => strtolower($name)]);
+                $data = [
+                    'uniqueId' => $user['uniqueId'],
+                    'date' => date('Y-m-d H:i:s'),
+                    'price' => $PALIER,
+                    'gateway' => 'starpass',
+                    'pseudo' => $name,
+                    'points' => $virtual_currency,
+                    //'transaction_id' => $code
+                ];
+
+                $this->container->mongo->funds->insertOne($data);
+                $money = $this->container->mongo->fund_list->findOne(["uniqueId" => $user['uniqueId']]);
+                if ($money == null){
+                    $data = [
+                        "uniqueId" => $user['uniqueId'],
+                        "points" => $dedipass->virtual_currency
+                    ];
+                    $this->container->mongo->fund_list->insertOne($data);
+                }else{
+                    $money['points'] = $money['points'] + $dedipass->virtual_currency;
+                    $this->container->mongo->fund_list->updateOne(["uniqueId" => $user['uniqueId']], ['$set' => ["points" => $money['points']]]);
+                }
+
+                if ($this->container->session->exist('user')){
+                    $mailContent = file_get_contents("../mail-achat.html");
+                    $mailContent = str_replace("(username)", $name, $mailContent);
+                    $mailContent = str_replace("(date)", date('Y-m-d H:i:s'), $mailContent);
+                    $mail = new \App\Mail(true);
+                    $mail->sendMail($this->session->get('user')["email"], "BadBlock - Rechargement", $mailContent);
+                }
+
         return $this->redirect($response, '/shop/recharge/success');
     }
 
