5e5ea44416e7c48e3ba75000c497e56df6d6054e
xMalware <xmalware2@gmail.com>
2018-08-23 22:48:00 +0200

    SEO

--- App/Controllers/VoteController.php
+++ App/Controllers/VoteController.php
@@ -31,7 +31,7 @@ class VoteController extends Controller
         }
 
         try {
-            $mailContent = file_get_contents("../../public/dist/mails/mail-achat.html");
+            $mailContent = file_get_contents("https://badblock.fr/dist/mails/mail-achat.html");
             $mailContent = str_replace("(username)", $player, $mailContent);
             $mailContent = str_replace("(date)", date('Y-m-d H:i:s'), $mailContent);
         }
