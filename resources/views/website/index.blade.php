f7d1452ae63439a839691358b9b7c14814b98274
Mathieu <mathieu.richard31@orange.fr>
2018-09-16 18:21:30 +0200

    fifi

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -5,75 +5,7 @@ tps / ram / processeur / nb de joueurs
     <!-- Start content -->
     <div class="content">
         <div class="container">
-
             <div class="row">
-                <div class="col-lg-3 col-md-6">
-                    <div class="card-box">
-                        <div class="dropdown pull-right">
-                            <a href="#" class="dropdown-toggle card-drop" data-toggle="dropdown" aria-expanded="false">
-                                <i class="zmdi zmdi-more-vert"></i>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                            </ul>
-                        </div>
-
-                        <h4 class="header-title m-t-0 m-b-30">C.A Jour</h4>
-
-                        <div class="widget-box-2">
-                            <div class="widget-detail-2">
-                                <span class="badge badge-success pull-left m-t-20">0% <i class="zmdi zmdi-trending-up"></i> </span>
-                                <h2 class="m-b-0"> {{ $data['ca_d'] }}€ </h2>
-                                <p class="text-muted m-b-25">Chiffre entrant journalier</p>
-                            </div>
-                            <div class="progress progress-bar-success-alt progress-sm m-b-0">
-                                <div class="progress-bar progress-bar-success" role="progressbar"
-                                     aria-valuenow="77" aria-valuemin="0" aria-valuemax="100"
-                                     style="width: 77%;">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- end col -->
-
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="card-box">
-                        <div class="dropdown pull-right">
-                            <a href="#" class="dropdown-toggle card-drop" data-toggle="dropdown" aria-expanded="false">
-                                <i class="zmdi zmdi-more-vert"></i>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                            </ul>
-                        </div>
-
-                        <h4 class="header-title m-t-0 m-b-30">C.A Mois</h4>
-
-                        <div class="widget-box-2">
-                            <div class="widget-detail-2">
-                                <span class="badge badge-success pull-left m-t-20">32% <i class="zmdi zmdi-trending-up"></i> </span>
-                                <h2 class="m-b-0"> {{ $data['ca_m'] }}€</h2>
-                                <p class="text-muted m-b-25">Chiffre entrant mois</p>
-                            </div>
-                            <div class="progress progress-bar-success-alt progress-sm m-b-0">
-                                <div class="progress-bar progress-bar-success" role="progressbar"
-                                     aria-valuenow="77" aria-valuemin="0" aria-valuemax="100"
-                                     style="width: 77%;">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- end col -->
-
                 <div class="col-lg-3 col-md-6">
                     <div class="card-box">
                         <h4 class="header-title m-t-0 m-b-30">Décaissement Pts / Jour</h4>
@@ -160,13 +92,6 @@ tps / ram / processeur / nb de joueurs
                         </div>
                     </div>
                 </div><!-- end col -->
-
-                <div class="col-lg-8">
-                    <div class="card-box">
-                        <h4 class="header-title m-t-0">Créditation sur le mois</h4>
-                        <div id="cred" style="min-width: 310px; height: 300px; margin: 0 auto"></div>
-                    </div>
-                </div><!-- end col -->
                 <div class="col-lg-12">
                     <div class="card-box">
                         <h4 class="header-title m-t-0">Décaissement sur le mois en cours</h4>
@@ -215,15 +140,7 @@ tps / ram / processeur / nb de joueurs
             },
 
                 MorrisCharts.prototype.init = function() {
-
-                    //create line chart
-                    var $data  = [
-                        @foreach($data['ca_chart'] as $row)
-                            { y: '{{ $row['date'] }}', a: {{ $row['result'] }}, b: {{ $row['result_last'] }} },
-                        @endforeach
-                    ];
-                    this.createLineChart('cred', $data, 'y', ['a', 'b'], ['Mois en cours', 'Mois dernier'],['0.1'],['#ffffff'],['#999999'], ['#ff8acc', '#5b69bc']);
-
+                    
                     var $data  = [
                             @foreach($data['op_chart'] as $row)
                         { y: '{{ $row['date'] }}', a: {{ $row['result'] }}, b: {{ $row['result_last'] }} },
