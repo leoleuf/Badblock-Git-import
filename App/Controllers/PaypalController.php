efe7e076e62fad8a5eabb0d4a39d1aca78b2b6e4
Mathieu <mathieu.richard31@orange.fr>
2018-09-27 17:25:20 +0200

    debug

--- App/Controllers/PaypalController.php
+++ App/Controllers/PaypalController.php
@@ -105,7 +105,7 @@ class PaypalController extends Controller
             'TOKEN' => $_GET['token']
         ));
 
-        return var_dump("ddd". $resp);
+        return var_dump($resp);
 
         if($resp){
             if($resp['CHECKOUTSTATUS'] !== 'PaymentActionNotInitiated'){
