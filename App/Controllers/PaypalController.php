98950b1ad4253bb77f63dbd76392ba2afbbb0915
Mathieu <mathieu.richard31@orange.fr>
2018-09-27 18:34:26 +0200

    debug

--- App/Controllers/PaypalController.php
+++ App/Controllers/PaypalController.php
@@ -75,7 +75,7 @@ class PaypalController extends Controller
 
     public function process(RequestInterface $request, ResponseInterface $response){
 
-        return var_dump($request);
+        return var_dump($request->get('token'));
 
         if(!isset($_GET['offer']) || !isset($_GET['Prix']) || !isset($_GET['Offer']) || !isset($_GET['Offer_desc']) || !isset($_GET['Currency']) || !isset($_GET['QTY'])){
             return $this->redirect($response, '/shop/recharge/cancel#4');
