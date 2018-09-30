d785ad091fd8295a4bcde0aae6973dd8c738a71b
Mathieu <mathieu.richard31@orange.fr>
2018-09-30 14:17:43 +0200

    ko

--- cron.sh
+++ cron.sh
@@ -5,9 +5,7 @@
 while true; do
 
 curl https://badblock.fr/api/update-server-graph
-<<<<<<< HEAD
-=======
 curl https://badblock.fr/api/all-posts
->>>>>>> 73b26651076b20a4510368e85df2e23f05d888ef
+
 sleep 5
 done
