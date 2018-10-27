6d43ba060624737d21a1d948c8b5e45615686b9a
Mathieu <mathieu.richard31@orange.fr>
2018-10-27 12:06:45 +0200

    Marre du  ping bloquer

--- App/Controllers/Api/MinecraftApiController.php
+++ App/Controllers/Api/MinecraftApiController.php
@@ -13,17 +13,20 @@ class MinecraftApiController extends \App\Controllers\Controller
 {
 
 	public function getPlayers(ServerRequestInterface $request, ResponseInterface $response){
-        if ($this->container->redis->exists('api.mc.players') == 1 && intval($this->container->redis->get('api.mc.players')) != 0){
+        if ($this->container->redis->exists('api.mc.players') == 1 && intval($this->container->redis->get('api.mc.players')) != 0 && intval($this->container->redis->get('api.mc.players')) != null){
             return $response->withJson(["players" => ['now' => intval($this->container->redis->get('api.mc.players'))]]);
         }else{
             try
             {
                 $Query = new MinecraftPing( 'play.badblock.fr', 25565 );
-
                 $online = $Query->Query()["players"]["online"];
 
-                if (intval($online) != 0){
+                if (intval($online) != 0 && intval($online) != null){
                     $this->container->redis->set('api.mc.players', $online);
+                    $this->container->redis->expire('api.mc.players', 5);
+                }else{
+                    $online = file_get_contents('https://minecraft-api.com/api/ping/playeronline.php?ip=play.badblock.fr&port=25565');
+                    $this->container->redis->set('api.mc.players', intval($online));
                     $this->container->redis->expire('api.mc.players', 5);
                 }
 
