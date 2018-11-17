dad93cf12f3c3197f690649ccd6c54cb993ac2ff
xMalware <xmalware2@gmail.com>
2018-11-17 17:34:00 +0100

    ok

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -20,53 +20,40 @@
                         <table class="table">
                             <thead>
                             <tr>
-                                <th>Ref°</th>
-                                <th>Nom du serveur</th>
-                                <th>IP</th>
-                                <th>Site Web</th>
-                                <th>État</th>
-                                <th>Type de vérification de vote</th>
+                                <th>Serveur</th>
+                                <th>Type</th>
                                 <th>Action</th>
                             </tr>
                             </thead>
                             <tbody>
                             @foreach($server as $row)
                                 <tr>
-                                    <td>{{ $row->id }}</td>
-                                    <td>{{ $row->name }}</td>
-                                    <td>{{ $row->ip }}</td>
-                                    <td>{{ $row->website }}</td>
-                                    <td>
+                                    <td>{{ $row->name }}<br /><br />
                                         @if($row->actived)
                                             <span style="background-color: #0fff21" class="badge badge-dark">Actif</span>
                                         @else
                                             <span style="background-color: #ff100b" class="badge badge-dark">En attente</span>
                                         @endif
                                     </td>
-                                    <td>{{ $row->votetype }}</td>
                                     <td>
                                         <!--<button title="Supprimer le serveur {{ $row->name }}" onclick="location.href = '/dashboard/del-server/{{ $row->id }}';" type="button" role="button" class="btn btn-danger">
                                             <i class="mdi mdi-close-circle"></i>
                                         </button>!-->
                                         &nbsp;
                                         <button title="Modifier le serveur {{ $row->name }}" onclick="location.href = '/dashboard/edit-server/{{ $row->id }}';" type="button" role="button" class="btn btn-default">
-                                            <i class="mdi mdi-border-color"></i>
+                                            <i class="mdi mdi-border-color"></i> Modifier mon serveur
                                         </button>
                                             &nbsp;
                                             <button title="Intégrer à votre site" onclick="location.href = '/dashboard/api/{{ $row->id }}';" type="button" role="button" class="btn btn-default">
-                                                <i class="mdi mdi-code-tags-check"></i>
-                                            </button>
-                                            &nbsp;
-                                            <button title="Derniers votes du serveur" onclick="location.href = '/dashboard/votes/{{ $row->id }}';" type="button" role="button" class="btn btn-default">
-                                                <i class="mdi mdi mdi-database-plus"></i>
+                                                <i class="mdi mdi-code-tags-check"></i> Intégrer le vote<br />à votre serveur
                                             </button>
                                             &nbsp;
                                             <button title="Statistiques du serveur" onclick="location.href = '/dashboard/stats/{{ encname($row->name) }}';" type="button" role="button" class="btn btn-default">
-                                                <i class="mdi mdi mdi-gauge"></i>
+                                                <i class="mdi mdi mdi-gauge"></i> Statistiques du serveur
                                             </button>
                                             &nbsp;
                                             <button title="Accéder à la fiche du serveur" onclick="location.href = '/{{ encname($row->cat) }}/{{ encname($row->name) }}';" type="button" role="button" class="btn btn-default">
-                                                <i class="mdi mdi-newspaper"></i>
+                                                <i class="mdi mdi-newspaper"></i> Fiche du serveur
                                             </button>
                                     </td>
                                 </tr>
