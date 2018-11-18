c2a7d3d4682e58db3771f00b9117f8891b5a4eb0
xMalware <xmalware2@gmail.com>
2018-11-18 12:21:01 +0100

    ok

--- resources/views/front/vote.blade.php
+++ resources/views/front/vote.blade.php
@@ -160,7 +160,7 @@
                                         @endif
 
                                             <div id="bma" style="display: none;">
-                                                <a href="/minecraft/{{ encname($data->cat) }}/{{ encname($data->name) }}/vote" class="btn btn-error" style="width: 100%; height: 50px;">
+                                                <a href="/minecraft/{{ encname($data->cat) }}/{{ encname($data->name) }}/vote" class="btn btn-danger" style="height: 50px;">
                                                     Afin de fournir des informations précises aux créateurs, veuillez désactiver votre bloqueur de publicité.
                                                 </a>
                                             </div>
