ffc51beae0bed893d2510ac61e89bcf61b32f801
xMalware <xmalware2@gmail.com>
2018-11-18 13:30:43 +0100

    ok

--- resources/views/front/vote.blade.php
+++ resources/views/front/vote.blade.php
@@ -168,7 +168,7 @@
                                             <div id="blox" class="col-12" style="display: none; height: 50px; z-index: 100;"></div>
                                         <button class="g-recaptcha btn btn-success col-12" id="vote_button"
                                                 data-sitekey="6Lf8amQUAAAAAM2wJE-R24huo1IDSTgDQZVoURX1"
-                                                data-callback="onSubmit" style="display: none; height: 50px; z-index:-100;" disabled>
+                                                data-callback="onSubmit" style="display: none; height: 50px; margin-top: -5px;" disabled>
                                             Voter <i class="lnr lnr-arrow-right"></i>
                                         </button>
                                         {{ csrf_field() }}
