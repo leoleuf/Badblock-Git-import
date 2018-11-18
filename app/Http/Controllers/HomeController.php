85aadd6e9bea6446df041cfb1b5e0dee9b2dcb14
xMalware <xmalware2@gmail.com>
2018-11-18 22:06:26 +0100

    ok

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -80,54 +80,6 @@ class HomeController extends Controller
 
     public function category($cat)
     {
-        try {
-// THE FOLLOWING BLOCK IS USED TO RETRIEVE AND DISPLAY LINK INFORMATION.
-// PLACE THIS ENTIRE BLOCK IN THE AREA YOU WANT THE DATA TO BE DISPLAYED.
-
-// MODIFY THE VARIABLES BELOW:
-// The following variable defines whether links are opened in a new window
-// (1 = Yes, 0 = No)
-            $OpenInNewWindow = "1";
-
-// # DO NOT MODIFY ANYTHING ELSE BELOW THIS LINE!
-// ----------------------------------------------
-            $BLKey = "4W76-I790-THB2";
-
-            if (isset($_SERVER['SCRIPT_URI']) && strlen($_SERVER['SCRIPT_URI'])) {
-                $_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_URI'] . ((strlen($_SERVER['QUERY_STRING'])) ? '?' . $_SERVER['QUERY_STRING'] : '');
-            }
-
-            if (!isset($_SERVER['REQUEST_URI']) || !strlen($_SERVER['REQUEST_URI'])) {
-                $_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'] . ((isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING'])) ? '?' . $_SERVER['QUERY_STRING'] : '');
-            }
-
-            $QueryString = "LinkUrl=" . urlencode(((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-            $QueryString .= "&Key=" . urlencode($BLKey);
-            $QueryString .= "&OpenInNewWindow=" . urlencode($OpenInNewWindow);
-
-            $lnk = "";
-            if (intval(get_cfg_var('allow_url_fopen')) && function_exists('readfile')) {
-                $lnk = @file_get_contents("http://www.backlinks.com/engine.php?" . $QueryString);
-            } elseif (intval(get_cfg_var('allow_url_fopen')) && function_exists('file')) {
-                if ($content = @file("http://www.backlinks.com/engine.php?" . $QueryString))
-                    $lnk .= @join('', $content);
-            } elseif (function_exists('curl_init')) {
-                $ch = curl_init("http://www.backlinks.com/engine.php?" . $QueryString);
-                curl_setopt($ch, CURLOPT_HEADER, 0);
-                curl_exec($ch);
-
-                if (curl_error($ch))
-                    $lnk = "er1";
-
-                curl_close($ch);
-            } else {
-                $lnk = "er2";
-            }
-        }
-        catch (Exception $n)
-        {
-            $lnk = "er3";
-        }
 
         $catName = strtolower($cat);
         $l = array();
@@ -174,10 +126,10 @@ class HomeController extends Controller
 
         if ($this->isABot())
         {
-            return view('front.category', ['about' => $about, 'bot' => true, 'votelistok' => $votelistok, 'tags' => $tagsInfo, 'catName' => $catName, 'data' => $data, 'current_page' => 1 ,'page_number' => $nb, 'lnk' => $lnk]);
+            return view('front.category', ['about' => $about, 'bot' => true, 'votelistok' => $votelistok, 'tags' => $tagsInfo, 'catName' => $catName, 'data' => $data, 'current_page' => 1 ,'page_number' => $nb, 'lnk' => '']);
         }
 
-        return view('front.category', ['about' => $about, 'votelistok' => $votelistok, 'tags' => $tagsInfo, 'catName' => $catName, 'data' => $data, 'current_page' => 1 ,'page_number' => $nb, 'lnk' => $lnk]);
+        return view('front.category', ['about' => $about, 'votelistok' => $votelistok, 'tags' => $tagsInfo, 'catName' => $catName, 'data' => $data, 'current_page' => 1 ,'page_number' => $nb, 'lnk' => '']);
     }
 
     public function faq()
