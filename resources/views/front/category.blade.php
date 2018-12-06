ac6d2c05fffdbdd0db619f0ee21cd68f1997b4d9
xMalware <xmalware2@gmail.com>
2018-12-06 23:01:00 +0100

    ok

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -87,6 +87,7 @@
                         @endif
                     </div>
                 </div>
+
             </div>
         </section>
         <!-- End banner Area -->
@@ -128,11 +129,13 @@
                         @endif
                         @if (isset($catName) && $catName == "clash-of-clans")
                                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                <!-- serveur-multigames -->
                                 <ins class="adsbygoogle"
                                      style="display:block"
-                                     data-ad-format="autorelaxed"
                                      data-ad-client="ca-pub-1905923613312160"
-                                     data-ad-slot="4273456239"></ins>
+                                     data-ad-slot="1434308007"
+                                     data-ad-format="auto"
+                                     data-full-width-responsive="true"></ins>
                                 <script>
                                     (adsbygoogle = window.adsbygoogle || []).push({});
                                 </script>
