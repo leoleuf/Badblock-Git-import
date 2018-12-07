bc19decdcd95ac38729c1e3efc7e9c0388393c4c
Mathieu <mathieu.richard31@orange.fr>
2018-12-07 21:21:47 +0100

    ok

--- cron.sh
+++ cron.sh
@@ -6,26 +6,26 @@ while true; do
 
 curl https://badblock.fr/api/update-server-graph
 
-sleep 1
+sleep 10
 
 curl https://badblock.fr/api/cache/all-posts
 
-sleep 1
+sleep 10
 
 curl https://badblock.fr/api/minecraft/players
 
-sleep 1
+sleep 10
 
 curl https://badblock.fr/api/stats/json
 
-sleep 1
+sleep 10
 
 curl https://badblock.fr/api/cache/all-staff
 
-sleep 1
+sleep 10
 
 curl https://badblock.fr/api/cache/all-stats
 
-sleep 1
+sleep 10
 
 done
