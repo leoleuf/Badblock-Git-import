bf5d12456b44dd2f9bb0a9ec9bc715c97b7903fa
xMalware <xmalware2@gmail.com>
2018-12-15 10:27:32 +0100

    ok

--- resources/views/front/mise-en-avant.blade.php
+++ resources/views/front/mise-en-avant.blade.php
@@ -28,10 +28,11 @@
                 <h3 class="mb-30">Mettre en avant son serveur</h3>
                 <div class="row">
                     <div class="col-md-3">
-                        <img src="img/elements/d.jpg" alt="" class="img-fluid">
+                        <img alt="Mise en avant" src="img/grow.jpg" class="img-fluid">
                     </div>
                     <div class="col-md-9 mt-sm-20">
                         <p>Envie de <strong>gagner plus de visibilité</strong> et plus de joueurs sur ton serveur de jeu privé ? Avec Serveur MultiGames, il est possible de mettre en avant ton serveur en quelques clics seulement depuis le Tableau de Bord afin d'intéresser la communauté du jeu et de la rediriger vers ton serveur pour le <strong>faire connaître un maximum</strong> en seulement quelques étapes. <strong>Prends la première place</strong> sans plus hésiter.<br />
+                            <br />
                             @if(Auth::user())
                                 <a title="Mettre en avant mon serveur" href="/dashboard/mise-en-avant" class="genric-btn danger circle arrow">Mettre en avant mon serveur<span class="lnr lnr-arrow-right"></span></a>
                             @else
