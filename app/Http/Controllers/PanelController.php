9c495e3e86695d5c574d125c940fb830177f8bf4
xMalware <xmalware2@gmail.com>
2018-12-22 11:43:33 +0100

    ok

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -6,6 +6,7 @@ use Image;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Storage;
 
 
