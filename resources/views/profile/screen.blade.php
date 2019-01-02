d5790ab1941e8a634d7d4de21de675bf7c577c4c
Mathieu <mathieu.richard31@orange.fr>
2019-01-02 22:58:51 +0100

    oups

--- resources/views/profile/screen.blade.php
+++ resources/views/profile/screen.blade.php
@@ -4,33 +4,27 @@
     <link rel="stylesheet" href="/assets/plugins/magnific-popup/dist/magnific-popup.css"/>
 @endsection
 @section('content')
-    <div class="content-page">
-        <!-- Start content -->
-        <div class="content">
-            <div class="container">
-                <div class="row">
-                    <div class="portfolioContainer">
-                        @foreach($Screen as $row)
-                            <div class="col-md-6 col-xl-3 col-lg-4 natural personal">
-                                <div class="gal-detail thumb">
-                                    <a href="https://images.badblock.fr/i/{{ $row['file_name'] }}" class="image-popup" title="Screenshot-1">
-                                        <img src="https://images.badblock.fr/i/{{ $row['file_name'] }}" class="thumb-img" alt="work-thumbnail">
-                                    </a>
-                                    <h5>{{ $row['file_name'] }} - {{ $row['date'] }} - {{ $row['ip'] }}</h5>
-                                </div>
-                            </div>
-                        @endforeach
+    <div class="row">
+        <div class="portfolioContainer">
+            <div class="row">
+                @foreach($Screen as $row)
+                    <div class="col-md-4 col-xl-6 col-lg-6 natural personal">
+                        <div class="gal-detail thumb">
+                            <a href="https://images.badblock.fr/i/{{ $row['file_name'] }}" class="image-popup" title="Screenshot-1">
+                                <img src="https://images.badblock.fr/i/{{ $row['file_name'] }}" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <h5>{{ $row['file_name'] }} - {{ $row['date'] }} - {{ $row['ip'] }}</h5>
+                        </div>
                     </div>
-                </div>
-                <ul class="pagination pagination-split">
-                    @for($i = 1; $i <= $Page ; $i++)
-                        <li class="page-item"><a class="page-link" href="/screen/{{ $i }}">{{ $i }}</a></li>
-                    @endfor
-                </ul>
+                @endforeach
             </div>
         </div>
-
-
+    </div>
+    <ul class="pagination pagination-split">
+        @for($i = 1; $i <= $Page ; $i++)
+            <li class="page-item"><a class="page-link" href="/screen/{{ $i }}">{{ $i }}</a></li>
+        @endfor
+    </ul>
 @endsection
 @section('after_scripts')
     <script type="text/javascript" src="/assets/plugins/isotope/dist/isotope.pkgd.min.js"></script>
