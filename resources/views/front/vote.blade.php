fecb87ac30a28c1a8330621031063f9d76a68fdb
xMalware <xmalware2@gmail.com>
2019-01-04 10:32:48 +0100

    noredirect

--- resources/views/front/vote.blade.php
+++ resources/views/front/vote.blade.php
@@ -181,11 +181,11 @@
                                                 </a>
                                             </div>
                                         @if (!isMobile())
-                                            <div id="blox" class="col-12" style="display: none; height: 80px; z-index: 100;"></div>
+                                            <div id="blox" class="col-12" style="display: none; height: 80px; margin-bottom: -20px; z-index: 100;"></div>
                                         @else
                                         @endif
 
-                                        <button class="g-recaptcha btn btn-success col-12" id="vote_button"
+                                        <button class="g-recaptcha btn btn-success col-8" id="vote_button"
                                                 data-sitekey="6Lf8amQUAAAAAM2wJE-R24huo1IDSTgDQZVoURX1"
                                                 data-callback="onSubmit" style="display: none; height: 50px;" disabled>
                                             Voter <i class="lnr lnr-arrow-right"></i>
