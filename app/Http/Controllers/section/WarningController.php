ef8f88fb13ef4c8b6bd5371113d880e2384a0849
Hoooki <hooki.kibo@gmail.com>
2019-01-06 15:22:34 +0100

    profil

--- app/Http/Controllers/section/WarningController.php
+++ app/Http/Controllers/section/WarningController.php
@@ -19,7 +19,14 @@ use App\Http\Controllers\section\NotificationsController;
 class WarningController extends Controller
 {
 
-    public function index(){
+    public static function convertTime($date)
+    {
+        $DateTime = date_create($date);
+        return date_format($DateTime, 'd/m/Y à H:i:s');
+    }
+
+    public function index()
+    {
 
         return view('section.warning.send_warning');
 
@@ -27,60 +34,51 @@ class WarningController extends Controller
 
     public function list()
     {
-      return view('section.warning.list');
+        return view('section.warning.list');
     }
 
     public function display($id)
     {
 
-      $user = DB::table('warning')->where('id', '=', $id)->get();
-      if($user[0]->pseudo == Auth::user()->name)
-      {
-        return view('section.warning.display', ['user' => $user]);
-      }
-      else {
-        return redirect('/');
-      }
+        $user = DB::table('warning')->where('id', '=', $id)->get();
+        if ($user[0]->pseudo == Auth::user()->name) {
+            return view('section.warning.display', ['user' => $user]);
+        } else {
+            return redirect('/');
+        }
 
     }
 
     public function delete($id)
     {
-      DB::table('warning')->where('id', '=', $id)->delete();
-      return redirect('/section/avertissement-list');
+        DB::table('warning')->where('id', '=', $id)->delete();
+        return redirect('/section/avertissement-list');
     }
 
     public function send(Request $request)
     {
 
-      DB::table('warning')->insert([
+        DB::table('warning')->insert([
 
-        'warn_by' => $request->input('warn_by'),
-        'pseudo' => $request->input('pseudo'),
-        'title' => $request->input('title'),
-        'text' => $request->input('text')
+            'warn_by' => $request->input('warn_by'),
+            'pseudo' => $request->input('pseudo'),
+            'title' => $request->input('title'),
+            'text' => $request->input('text')
 
-      ]);
-
-
-      DB::table('notifications')->insert([
-        'user_id' => NotificationsController::convertPseudoId($request->input('pseudo')),
-        'title' => $request->input('title'),
-        'link' => '/section/avertissement/'.DB::table('warning')->max('id'),
-        'icon' => 'https://image.flaticon.com/icons/svg/179/179386.svg',
-        'text' => 'Vous venez de recevoir un avertissement.'
-      ]);
+        ]);
 
 
+        DB::table('notifications')->insert([
+            'user_id' => NotificationsController::convertPseudoId($request->input('pseudo')),
+            'title' => $request->input('title'),
+            'link' => '/avertissement/' . DB::table('warning')->max('id'),
+            'icon' => 'https://image.flaticon.com/icons/svg/179/179386.svg',
+            'text' => 'Vous venez de recevoir un avertissement.'
+        ]);
 
-      return redirect('/section/avertissement-list');
 
-    }
+        return redirect('/section/avertissement-list');
 
-    public static function convertTime($date)
-    {
-      $DateTime = date_create($date);
-      return date_format($DateTime, 'd/m/Y à H:i:s');
     }
 
 }
