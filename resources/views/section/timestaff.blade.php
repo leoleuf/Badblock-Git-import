25ee3ae5715716a4abfe0ec90b4e20fc430f52a2
Mathieu <mathieu.richard31@orange.fr>
2019-01-15 18:19:17 +0100

    okok

---
+++ resources/views/section/timestaff.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+@section('content')
+    <div class="content-page">
+        <!-- Start content -->
+        <div class="content">
+            <div class="container">
+                <h1>Temps de connection :</h1>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="panel">
+                            <div class="panel-body">
+                                    <div class="">
+                                        <table class="table table-striped" id="datatable">
+                                            <thead>
+                                            <tr>
+                                                <th>Pseudo</th>
+                                                <th>Temps de connection</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            @foreach($user as $row)
+                                                <tr class="gradeX">
+                                                    <td>{{ $row['name'] }}</td>
+                                                    <td>{{ date('H:i:s', $row['time']) }}</td>
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+@endsection
+@section("after_scripts")
+
+<!-- Editable js -->
+<script src="/assets/plugins/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
+<script src="/assets/plugins/jquery-datatables-editable/jquery.dataTables.js"></script>
+<script src="/assets/plugins/datatables/dataTables.bootstrap.js"></script>
+<script src="/assets/plugins/tiny-editable/mindmup-editabletable.js"></script>
+<script src="/assets/plugins/tiny-editable/numeric-input-example.js"></script>
+<!-- init -->
+<script src="/assets/pages/datatables.editable.init.js"></script>
+
+
+
+@endsection
