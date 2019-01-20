665303f48b5089438b657035ea50ec7ef728e7b9
xMalware <xmalware2@gmail.com>
2019-01-20 17:39:32 +0100

    ok

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -65,7 +65,7 @@ $d = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http")
         <link rel="apple-touch-icon" sizes="180x180" href="/img/mc/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/img/mc/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/img/mc/favicon-16x16.png">
-        <link rel="manifest" href="/img/mc/site.webmanifest">
+        <link rel="manifest" href="/site.webmanifest">
         <link rel="mask-icon" href="/img/mc/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#ffffff">
