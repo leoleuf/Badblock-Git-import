d56801932f044729fae43b06fe4900e9de80e0a3
xMalware <xmalware2@gmail.com>
2019-01-20 18:57:38 +0100

    ok

--- resources/views/front/vote.blade.php
+++ resources/views/front/vote.blade.php
@@ -198,7 +198,7 @@
                                 </script>
                         @else
                             <ins class="adsbygoogle"
-                                 style="display:inline-block;width:300px;height:60px"
+                                 style="display:inline-block;max-width: 60px;"
                                  data-ad-client="ca-pub-1905923613312160"
                                  data-ad-slot="4687571933"></ins>
                             <script>
