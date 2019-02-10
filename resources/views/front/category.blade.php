19ef3d0cdc8d920dfea36024ac857e2ddd8fe033
xMalware <xmalware2@gmail.com>
2019-02-10 01:29:34 +0100

    test

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -234,16 +234,14 @@
                                         (adsbygoogle = window.adsbygoogle || []).push({});
                                     </script>!-->
                                        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                                       <!-- serveurmultigames-headclassement -->
+                                       <!-- serveurmultigames-telecharger-sidebar-installer -->
                                        <ins class="adsbygoogle"
-                                            style="display:block"
+                                            style="display:inline-block;width:300px;height:600px"
                                             data-ad-client="ca-pub-1905923613312160"
-                                            data-ad-slot="8643178484"
-                                            data-ad-format="auto"
-                                            data-full-width-responsive="true"></ins>
+                                            data-ad-slot="8169956642"></ins>
                                        <script>
                                            (adsbygoogle = window.adsbygoogle || []).push({});
-                                       </script><br />
+                                       </script>
                                 @endif
                         @php($totalReviews = 0)
                         @php($average = 0)
