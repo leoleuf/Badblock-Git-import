35780863613165419a4d698573d33142c5038a1d
xMalware <xmalware2@gmail.com>
2019-02-10 01:23:01 +0100

    test

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -234,8 +234,16 @@
                                         (adsbygoogle = window.adsbygoogle || []).push({});
                                     </script>!-->
                                        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1905923613312160" data-ad-slot="1434308007" data-ad-format="auto" data-full-width-responsive="true"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-<br />
+                                       <!-- serveurmultigames-headclassement -->
+                                       <ins class="adsbygoogle"
+                                            style="display:block"
+                                            data-ad-client="ca-pub-1905923613312160"
+                                            data-ad-slot="8643178484"
+                                            data-ad-format="auto"
+                                            data-full-width-responsive="true"></ins>
+                                       <script>
+                                           (adsbygoogle = window.adsbygoogle || []).push({});
+                                       </script><br />
                                 @endif
                         @php($totalReviews = 0)
                         @php($average = 0)
