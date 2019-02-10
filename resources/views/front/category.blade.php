8d056e9fe38c82224320f1e22efe1a0d7996ab69
xMalware <xmalware2@gmail.com>
2019-02-10 01:17:42 +0100

    test

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -235,7 +235,7 @@
                                     </script>!-->
                                        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1905923613312160" data-ad-slot="1434308007" data-ad-format="auto" data-full-width-responsive="true"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-
+<br />
                                 @endif
                         @php($totalReviews = 0)
                         @php($average = 0)
