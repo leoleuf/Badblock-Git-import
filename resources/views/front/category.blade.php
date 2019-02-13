a464d0ac04f3bc12facb48a957d484865f8833cd
xMalware <xmalware2@gmail.com>
2019-02-13 16:32:33 +0100

    p

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -223,28 +223,6 @@
                                         @endif
                                     </div>
                                 @else
-                            @if (!_bot_detected())
-                                   <!-- <ins class="adsbygoogle"
-                                         style="display:block"
-                                         data-ad-client="ca-pub-1905923613312160"
-                                         data-ad-slot="1557301122"
-                                         data-ad-format="link"
-                                         data-full-width-responsive="true"></ins>
-                                    <script>
-                                        (adsbygoogle = window.adsbygoogle || []).push({});
-                                    </script>!-->
-                                       <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                                       <!-- serveurmultigames-headclassement -->
-                                       <ins class="adsbygoogle"
-                                            style="display:block"
-                                            data-ad-client="ca-pub-1905923613312160"
-                                            data-ad-slot="8643178484"
-                                            data-ad-format="auto"
-                                            data-full-width-responsive="true"></ins>
-                                       <script>
-                                           (adsbygoogle = window.adsbygoogle || []).push({});
-                                       </script><br />
-                                @endif
                         @php($totalReviews = 0)
                         @php($average = 0)
                         @php($averageCount = 0)
