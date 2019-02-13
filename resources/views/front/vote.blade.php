13154481236f392ab42960be377b22473c9e8265
xMalware <xmalware2@gmail.com>
2019-02-13 16:29:16 +0100

    p

--- resources/views/front/vote.blade.php
+++ resources/views/front/vote.blade.php
@@ -189,7 +189,6 @@
 
                     </div>
 
-                        @if (!isMobile())
                                 <ins class="adsbygoogle"
                                      style="display: block; height: 60px; max-width: 600px; max-height: 300px;" id="ad2"
                                      data-ad-client="ca-pub-1905923613312160"
@@ -199,17 +198,6 @@
                                 <script>
                                     (adsbygoogle = window.adsbygoogle || []).push({});
                                 </script>
-                        @else
-                            <ins class="adsbygoogle"
-                                 style="display: block; height: 60px; max-width: 75%; max-height: 300px;" id="ad2"
-                                 data-ad-client="ca-pub-1905923613312160"
-                                 data-ad-slot="4687571933"
-                                 data-ad-format="auto"
-                                 data-full-width-responsive="true"></ins>
-                            <script>
-                                (adsbygoogle = window.adsbygoogle || []).push({});
-                            </script>
-                        @endif
                         @if ($data->noredirect == 0)
                             <script async defer src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                             <ins class="adsbygoogle"
