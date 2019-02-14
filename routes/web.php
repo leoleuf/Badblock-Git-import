4735ffbfec675a19fabd558315a91c9af19f57ff
BuildTools <unconfigured@null.spigotmc.org>
2019-02-14 18:14:07 +0100

    Made the home page front-end

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,8 @@ Route::group([
 Route::group([
     'middleware' => ["auth"],
 ], function () {
-    Route::get('/', function () {return view('welcome');});
-    Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::get('/', 'HomeController@index')->name('home'); ;
 
 
 
