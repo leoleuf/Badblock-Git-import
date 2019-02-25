680a58c77f7cab2c705b832eef6c2f3edc78271d
Mathieu <mathieu.richard31@orange.fr>
2019-02-25 11:20:06 +0100

    caca

--- pull.php
+++ pull.php
@@ -1,8 +1,5 @@
 <?php
 
 shell_exec('git pull http://websyncbot:WAK2tRsB5jMzredQJ9uYxqyZwQUM6eYWTP74RJPM4rVvrCapLjf4F5SVHrqNkY4X@lusitania.badblock.fr/Website/managerV2.git');
-shell_exec('php composer.phar install');
-shell_exec('php composer.phar update');
-
 
 ?>
