28e2d8550d4203b3db98d9540e20a8c0e57d6ae4
Mathieu <mathieu.richard31@orange.fr>
2019-02-26 15:22:32 +0100

    pull

--- pull.php
+++ pull.php
@@ -1,7 +1,6 @@
 <?php
 
 shell_exec('git pull http://websyncbot:WAK2tRsB5jMzredQJ9uYxqyZwQUM6eYWTP74RJPM4rVvrCapLjf4F5SVHrqNkY4X@lusitania.badblock.fr/Website/badblock-website.git');
-shell_exec('php composer.phar install');
-shell_exec('php composer.phar update');
+
 
 ?>
