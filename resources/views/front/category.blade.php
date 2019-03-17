a27a828b2fee4e60a2b893da02c334a18af0c1f4
xMalware <xmalware2@gmail.com>
2019-03-17 17:36:40 +0100

    new design

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -434,7 +434,7 @@
                         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                         <!-- serveur-multigames -->
                         <ins class="adsbygoogle"
-                             style="display:inline-block;width:300px;height:600px;margin-left:20%;"
+                             style="display:inline-block;width:300px;height:600px;"
                              data-ad-client="ca-pub-1905923613312160"
                              data-ad-slot="2719469514"></ins>
                         <script>
