3cb9a82492a25b15c4eb72685dc5c0e3d75f3fa9
xMalware <xmalware2@gmail.com>
2019-03-17 16:21:05 +0100

    new design

--- resources/views/front/vote.blade.php
+++ resources/views/front/vote.blade.php
@@ -212,11 +212,9 @@
                         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                         <!-- serveur-multigames -->
                         <ins class="adsbygoogle"
-                             style="display:block"
+                             style="display:inline-block;width:300px;height:600px"
                              data-ad-client="ca-pub-1905923613312160"
-                             data-ad-slot="1434308007"
-                             data-ad-format="auto"
-                             data-full-width-responsive="true"></ins>
+                             data-ad-slot="2719469514"></ins>
                         <script>
                             (adsbygoogle = window.adsbygoogle || []).push({});
                         </script>
