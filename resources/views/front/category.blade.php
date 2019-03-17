22adc01d13c541be8804b916be90064652690561
xMalware <xmalware2@gmail.com>
2019-03-17 17:05:56 +0100

    new design

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -183,6 +183,17 @@
                                         </p>
                                     </div>
                                 </div>
+                                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                <!-- serveur-multigames -->
+                                <ins class="adsbygoogle"
+                                     style="display:block"
+                                     data-ad-client="ca-pub-1905923613312160"
+                                     data-ad-slot="1434308007"
+                                     data-ad-format="auto"
+                                     data-full-width-responsive="true"></ins>
+                                <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
                                 @php($adsDone = 0)
                                 @php($normalDone = 0)
 
@@ -301,9 +312,11 @@
                                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                                 <!-- serveur-multigames -->
                                 <ins class="adsbygoogle"
-                                     style="display:inline-block;width:300px;height:600px"
+                                     style="display:block"
                                      data-ad-client="ca-pub-1905923613312160"
-                                     data-ad-slot="2719469514"></ins>
+                                     data-ad-slot="1434308007"
+                                     data-ad-format="auto"
+                                     data-full-width-responsive="true"></ins>
                                 <script>
                                     (adsbygoogle = window.adsbygoogle || []).push({});
                                 </script>
