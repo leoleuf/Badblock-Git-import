97970c1e1f4e874508027ec38e185cf3d4fb56fb
xMalware <xmalware2@gmail.com>
2019-03-20 12:20:46 +0100

    p

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'dashboard/recharge-validate'
+        'dashboard/recharge-validate',
+        'pm',
     ];
 }
