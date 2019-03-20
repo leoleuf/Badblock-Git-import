fcbb7b8a6f27348e62c9c41443a9e19a39907d86
xMalware <xmalware2@gmail.com>
2019-03-20 11:38:28 +0100

    fix

--- src/fr/badblock/bungee/modules/staff/ModuleStaff.java
+++ src/fr/badblock/bungee/modules/staff/ModuleStaff.java
@@ -2,10 +2,8 @@ package fr.badblock.bungee.modules.staff;
 
 import fr.badblock.bungee.modules.staff.commands.ChatStaffCommand;
 import fr.badblock.bungee.modules.staff.commands.OnlineStaffCommand;
-import fr.badblock.bungee.modules.staff.commands.StaffCommand;
 import fr.badblock.bungee.modules.staff.listeners.PlayerDisconnectChatStaffListener;
 import fr.badblock.bungee.modules.staff.listeners.PlayerLoginChatStaffListener;
-import fr.badblock.bungee.modules.staff.listeners.StaffContactListener;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class ModuleStaff extends Plugin
@@ -16,11 +14,9 @@ public class ModuleStaff extends Plugin
 	{
 		new ChatStaffCommand(this);
 		new OnlineStaffCommand(this);
-		new StaffCommand(this);
 
 		new PlayerDisconnectChatStaffListener(this);
 		new PlayerLoginChatStaffListener(this);
-		new StaffContactListener(this);
 	}
 	
 }
