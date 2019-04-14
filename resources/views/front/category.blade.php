dec1ac1975365e48860dcf1e483c25ceb2041e6d
xMalware <xmalware2@gmail.com>
2019-04-14 20:30:43 +0200

    ok

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -237,7 +237,7 @@
                                                 @endif
                                             </div>
                                         </div>
-                                @endif
+                                    @else
                             @foreach($data as $row)
                                     @if ($adsDone == 0 && isset($row->ad))
                                         @php($adsDone = 1)
@@ -330,6 +330,7 @@
                                     </div>
                                 </div>
                                 @endforeach
+                                    @endif
                                 <h3 class="lis-font-weight-500 mt-4"><i class="fa fa-align-right pr-2 lis-f-14"></i> Jeu {{ $catName }} serveur</h3>
                                 <div class="card lis-brd-light wow fadeInUp mb-4">
                                     <div class="card-body p-4">
