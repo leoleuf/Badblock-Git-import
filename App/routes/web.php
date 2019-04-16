60413e83869388e29939a60dba1216335fc822a6
Hoooki <hooki.kibo@gmail.com>
2019-04-16 18:47:16 +0200

    bug pseudo

--- App/routes/web.php
+++ App/routes/web.php
@@ -200,11 +200,10 @@ $app->get('/link', \App\Controllers\LinkController::class . ':step1')->setName('
 $app->get('/link/{step}', \App\Controllers\LinkController::class . ':poststep')->setName('link-1')->add(new App\Middlewares\Auth\RequiredAuthMiddleware($container));
 $app->post('/link', \App\Controllers\LinkController::class . ':poststep')->setName('link-post');
 
-/*$app->get('/move', \App\Controllers\MoveController::class . ':step1')->setName('move-1')->add(new App\Middlewares\Auth\RequiredAuthMiddleware($container));
+$app->get('/move', \App\Controllers\MoveController::class . ':step1')->setName('move-1')->add(new App\Middlewares\Auth\RequiredAuthMiddleware($container));
 $app->get('/move/1/{uuid}', \App\Controllers\MoveController::class . ':process_step2')->add(new App\Middlewares\Auth\RequiredAuthMiddleware($container));
 $app->get('/move/2/{uuid}', \App\Controllers\MoveController::class . ':process_step4')->add(new App\Middlewares\Auth\RequiredAuthMiddleware($container));
-$app->post('/move', \App\Controllers\MoveController::class . ':poststep')->setName('move-post')->add(new App\Middlewares\Auth\RequiredAuthMiddleware($container));*/
-
+$app->post('/move', \App\Controllers\MoveController::class . ':poststep')->setName('move-post')->add(new App\Middlewares\Auth\RequiredAuthMiddleware($container));
 
 
 $app->get('/logout', function ($request, $response) {
