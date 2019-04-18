24c9ff0c88c9dc882bee0a6be325fe9acf884bea
xMalware <xmalware2@gmail.com>
2019-04-18 10:50:26 +0200

    p

--- resources/views/front/partenaires2.blade.php
+++ resources/views/front/partenaires2.blade.php
@@ -9,14 +9,5 @@
     gtag('config', 'UA-122426050-1');
 </script>
 
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- serveur-multigames -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:300px;height:600px;margin-left:20%;"
-     data-ad-client="ca-pub-1905923613312160"
-     data-ad-slot="2719469514"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
 Redirection...
-<META http-equiv="refresh" content="3;URL=https://serveur-multigames.ne/minecraft">
+<META http-equiv="refresh" content="10;URL=https://serveur-multigames.ne/minecraft">
