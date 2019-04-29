e3d87794139e88a68530ca5056895397b7933c90
xMalware <xmalware2@gmail.com>
2019-04-29 21:11:19 +0200

    p

--- resources/views/front/vote.blade.php
+++ resources/views/front/vote.blade.php
@@ -479,7 +479,7 @@
                             var difflastd = $.now() - lastd;
                             stel = timediff > 8000 ? 700 : timediff > 5000 ? 500 : timediff > 2000 ? 300 : timediff > 1200 ? 250 : 200;
                             var pom = getRandomInt(stel - 200) + 200;
-                            console.log("bot: " + difflastd + " - " + pom);
+                            console.log("bot: " + difflastd + " - " + pom + " / " + stel);
                             if (difflastd > pom && maxtimes < 10) {
                                 maxtimes = maxtimes + 1;
                                 zo = $.now();
