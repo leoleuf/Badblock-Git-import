c5aaee02e70b937431a19a52928ca00b5a6bae3d
Audran Doublet <audran.doublet@epita.fr>
2019-04-30 09:20:49 +0200

    commands: api: add CommandSpigot

---
+++ commands/src/main/java/fr/badblock/gameapi/commands/CommandSpigot.java
@@ -0,0 +1,48 @@
+package fr.badblock.gameapi.commands;
+
+import org.bukkit.command.CommandSender;
+
+/**
+ * Commande ou sous-commande Spigot.
+ * @author LeLanN
+ */
+public class CommandSpigot extends CommandNode<CommandSender>
+{
+	private String permission;
+
+	public CommandSpigot(String name, String description)
+	{
+		super(name, description);
+	}
+
+	/**
+	 * Définis la permission nécessaire pour éxécuter cette commande
+	 * @param permission
+	 */
+	public void setPermission(String permission)
+	{
+		this.permission = permission;
+	}
+
+	@Override
+	public boolean isAllowed(CommandSender source)
+	{
+		return super.isAllowed(source) && (permission == null || source.hasPermission(permission));
+	}
+
+	@Override
+	public CommandSender getSource(Object obj)
+	{
+		if (obj instanceof CommandSender) // < 1.13
+			return (CommandSender) obj;
+
+		try
+		{
+			return (CommandSender) obj.getClass().getMethod("getBukkitSender").invoke(obj);
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+}
