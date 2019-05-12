235f6ab15725a7de3b2a75e91b49135ea2023ed7
Hoooki <hooki.kibo@gmail.com>
2019-05-12 21:36:41 +0200

    warn

--- routes/web.php
+++ routes/web.php
@@ -108,8 +108,8 @@ Route::group([
         Route::get('/preuve/{id}', 'moderation\CasierController@preuve')->middleware("can:mod_proof");
         Route::get('/guardian/{id}', 'moderation\GuardianController@view')->middleware("can:mod_guardianer");
         //Search moderator
-        Route::get('/sanction/search', 'moderation\ModerationController@search_index')->middleware("can:mod_search_sanction");
-        Route::get('/sanction/search/{username}', 'moderation\ModerationController@search')->middleware("can:mod_search_sanction");
+        Route::get('/sanction/search', 'moderation\ModerationController@search_index')->middleware("can:mod_guardianer");
+        Route::get('/sanction/{username}', 'moderation\ModerationController@search')->middleware("can:mod_guardianer");
 
         //TX Sanction
         Route::get('/sanction-tx', 'moderation\SanctionController@index');
