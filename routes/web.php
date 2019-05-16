afecb498ad5085399e4531f0ef9021c6e0d20875
Noan Perrot <noan.perrot@gmail.com>
2019-05-16 20:06:06 +0200

    Update des perms

--- routes/web.php
+++ routes/web.php
@@ -108,8 +108,8 @@ Route::group([
         Route::get('/preuve/{id}', 'moderation\CasierController@preuve')->middleware("can:mod_proof");
         Route::get('/guardian/{id}', 'moderation\GuardianController@view')->middleware("can:mod_guardianer");
         //Search moderator
-        Route::get('/sanction/search', 'moderation\ModerationController@search_index')->middleware("can:mod_guardianer");
-        Route::get('/sanction/{username}', 'moderation\ModerationController@search')->middleware("can:mod_guardianer");
+        Route::get('/sanction/search', 'moderation\ModerationController@search_index')->middleware("can:mod_search_mod_sanction");
+        Route::get('/sanction/{username}', 'moderation\ModerationController@search')->middleware("can:mod_search_mod_sanction");
 
         //TX Sanction
         Route::get('/sanction-tx', 'moderation\SanctionController@index');
