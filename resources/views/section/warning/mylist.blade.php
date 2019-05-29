c12bf606a836b4221934d548811d82c2fe096ebb
Vanilor <vanilor.dev@gmail.com>
2019-05-29 21:37:37 +0200

    Warning user update

--- resources/views/section/warning/mylist.blade.php
+++ resources/views/section/warning/mylist.blade.php
@@ -15,7 +15,7 @@
                                     <th>Pseudo</th>
                                     <th>Avertis par</th>
                                     <th>Titre</th>
-                                    <th></th>
+                                    <th>Raison</th>
                                     <th>Date</th>
                                 </tr>
                                 </thead>
@@ -26,6 +26,30 @@
                                         <td>{{ $val->pseudo }}</td>
                                         <td>{{ $val->warn_by }}</td>
                                         <td>{{ $val->title }}</td>
+                                        <td>
+                                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ContentModal{{ $key }}">
+                                                <i class="fas fa-eye"></i> Voir
+                                            </button>
+
+                                            <div class="modal fade" id="ContentModal{{ $key }}" tabindex="-1" role="dialog" aria-labelledby="Sanction" aria-hidden="true">
+                                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title">Sanction</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            {{ $val->text }}
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
                                         <td>{{ \App\Http\Controllers\DateController::formatDate($val->created_at) }}</td>
                                     </tr>
                                 @endforeach
