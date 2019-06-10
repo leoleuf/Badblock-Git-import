cc0968af856c7033a3997b837343d4a4f7885d97
xMalware <xmalware2@gmail.com>
2019-06-10 12:17:06 +0200

    m

--- resources/views/front/lien.blade.php
+++ resources/views/front/lien.blade.php
@@ -26,15 +26,14 @@
                             <div class="col-md-12 mt-sm-40">
                                 <p>
                                     Les liens Internet constituent une chose importante dans notre environnement quotidien. En effet, le lien définit l'identité du site Internet que vous mettez en place, mais il est nécessaire d'y apporter un travail rigoureux, notamment pour les différents endroits de votre site, pour faciliter le référencement du site. <a title="Raccourcir un lien" href="https://monlien.net/">Raccourcir URL</a> est un bon compromis pour votre site Internet.
-                                    <br /><br />
+                                    <script async src="https://cdn.kiwys.com/build/kiwys.min.js"></script>
+                                    <ins class="kiwys-ads" data-ad-slot="10643"></ins>
                                     Aujourd'hui, de nombreux services n'acceptent pas les liens longs et les URL non conformes. MonLien.net est un site Internet qui permet de raccourcir un lien et de suivre les statistiques d'accès à celui-ci (nombre de clics et de conversions). C'est pourquoi il est aujourd'hui judicieux d'utiliser un service comme cela afin de faire passer des URL moins longues pour pouvoir les échanger plus facilement sur la toile.
                                     <br /><br />
                                     En revanche, dans quelque chose, il y faut toujours une double utilité. Ainsi, il est possible de monétiser son URL avec MonLien.net et d'effectuer des demandes de paiement en quelques clics seulement. Avec son système sécurisé, MonLien.net redonne de l'importance à la réduction des URL, et s'engage à payer les créateurs de liens en fonction de l'affluence des URL créées. MonLien.net respecte également la vie privée de ses utilisateurs en ne communiquant pas les données qui peuvent être récoltées (horodatages et adresses IP). Les données sont par ailleurs supprimées au bout de vingt-quatre heures des serveurs de données du site Internet.
                                     <br /><br />
                                     Un lien doit être en dur, c'est un lien hypertexte représenté en langage HTML. Le lien de qualité se distingue par le fait qu'il apporte en référencement. Le lien est en dur si vous pouvez le survoler avec le pointeur de la souris et qu'une animation s'affiche. Il doit être accompagné de mots clés.
                                     <br /><br />
-                                    <script async src="https://cdn.kiwys.com/build/kiwys.min.js"></script>
-                                    <ins class="kiwys-ads" data-ad-slot="10595"></ins>
                                 </p>
                             </div>
             </div>
