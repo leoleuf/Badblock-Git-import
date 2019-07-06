93ceef775503664bcc2940dfdc91b31c7f694bc9
xMalware <xmalware2@gmail.com>
2019-07-06 14:02:21 +0200

    p

--- resources/views/front/vote.blade.php
+++ resources/views/front/vote.blade.php
@@ -564,7 +564,7 @@
 
                             var timediff = $.now() - zo;
                             var bdiff = $.now() - Math.max(tx, ty);
-                            var zolv = timediff > 8000 ? 300 : timediff > 5000 ? 200 : timediff > 2000 ? 100 : timediff > 1200 ? 75 : 0;
+                            var zolv = timediff > 8000 ? 300 : timediff > 5000 ? 200 : timediff > 2000 ? 300 : timediff > 1200 ? 275 : 250;
 
                             dbg = zolv + " : " + bdiff + " - Times: " + maxtimes + "/10";
                             if (bdiff > zolv && maxtimes < 15)
