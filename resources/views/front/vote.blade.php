833ead222017ac3354c0013ca07b16a0f5ac84e1
xMalware <xmalware2@gmail.com>
2019-07-10 19:15:34 +0200

    p

--- resources/views/front/vote.blade.php
+++ resources/views/front/vote.blade.php
@@ -641,10 +641,4 @@
             </script>
 
         @endif
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-4636627444279583",
-            enable_page_level_ads: true
-        });
-    </script>
 @endsection
