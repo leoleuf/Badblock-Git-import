ae6b6ff50f008911a5e9ad0fca16e5b274ac54ef
xMalware <xmalware2@gmail.com>
2019-07-11 20:13:22 +0200

    p

--- resources/views/front/vote.blade.php
+++ resources/views/front/vote.blade.php
@@ -560,7 +560,7 @@
                             var zolv = timediff > 8000 ? 300 : timediff > 5000 ? 200 : timediff > 2000 ? 300 : timediff > 1200 ? 275 : 250;
 
                             dbg = zolv + " : " + bdiff + " - Times: " + maxtimes + "/10";
-                            if (bdiff > zolv && maxtimes < 15)
+                            if (bdiff > zolv && maxtimes < 7)
                             {
                                 maxtimes = maxtimes + 1;
                                 lastd = $.now();
@@ -573,7 +573,7 @@
                         if (document.getElementById("vb").style.marginTop == "60px")
                         {
                             var difflastd = $.now() - lastd;
-                            if (difflastd > 1000 && maxtimes < 10)
+                            if (difflastd > 1000 && maxtimes < 7)
                             {
                                 maxtimes = maxtimes + 1;
                                 lastd = $.now();
