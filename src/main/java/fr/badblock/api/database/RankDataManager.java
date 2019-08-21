c1b60633ea7e7096c0bca33c2af83da5a4a0a68e
Hugo Planque <T4R13N@gmail.com>
2019-08-21 20:32:49 +0200

    test

--- src/main/java/fr/badblock/api/database/RankDataManager.java
+++ src/main/java/fr/badblock/api/database/RankDataManager.java
@@ -1,8 +1,13 @@
 package fr.badblock.api.database;
 
+import com.google.gson.JsonObject;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import fr.badblock.api.BadBlockAPI;
 import fr.badblock.api.data.rank.RankBean;
+
+import java.util.List;
 
 public class RankDataManager {
 
@@ -14,7 +19,21 @@ public class RankDataManager {
 
     public RankBean getRank(long rankId, RankBean rankBean) {
         try{
-
+            DBObject dbObject = new BasicDBObject("rankId", rankId);
+            DBObject found = ranks.findOne(dbObject);
+            if(found != null){
+                String rankName = (String) found.get("rankName");
+                int rankPower = (int) found.get("rankPower");
+                String rankTag = (String) found.get("rankTag");
+                String rankPrefix = (String) found.get("rankPrefix");
+                String rankSuffix = (String) found.get("rankSuffix");
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.add("s", "s");
+                List<JsonObject> rankPermissions;
+            }else{
+                this.createRank(rankBean);
+                return this.getRank(rankId, rankBean);
+            }
         }
     }
 
