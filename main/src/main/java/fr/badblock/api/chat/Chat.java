7740b041999c327ea7c1be0201978c4638cc149d
Cixxor <cixxordev@gmail.com>
2019-09-09 15:44:29 +0200

    Aération du code

--- main/src/main/java/fr/badblock/api/chat/Chat.java
+++ main/src/main/java/fr/badblock/api/chat/Chat.java
@@ -9,26 +9,40 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+/**
+*@author Cixxor
+**/
+
 public class Chat implements Listener {
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e) {
         Player p = e.getPlayer();
+
         PlayerData playerData = BadBlockAPI.getPluginInstance().getPlayerManager().getPlayerData(p.getName());
+
         RankData rankData = BadBlockAPI.getPluginInstance().getRankManager().getRankData(playerData.getRankID());
+
         /** Setting str as the string present in a config.yml file when the chat is muted **/
         String str = BadBlockAPI.getPluginInstance().getConfig().getString("chat.messageondisabled");
+        
         /** Cancelling chat event when the chat is disabled **/
         if (!ChatCommand.isActivated) {
             p.sendMessage(ChatUtilities.f(str));
             e.setCancelled(true);
-        } else {
+        }
+        
+         else {
             /** Else, we don't care about it **/
             e.setCancelled(false);
+
             if (rankData.isStaff()) {
                 e.setFormat(ChatUtilities.f(rankData.getRankPrefix() + rankData.getRankName()) + " " + playerData.getNormalName() + " " + ChatUtilities.f(rankData.getRankSuffix()) + ChatColor.DARK_GRAY + "> " + ChatColor.WHITE + ChatUtilities.f(e.getMessage()));
-            } else {
+            }
+            
+             else {
                 e.setFormat(ChatUtilities.f(rankData.getRankPrefix() + rankData.getRankName()) + " " + playerData.getNormalName() + " " + ChatUtilities.f(rankData.getRankSuffix()) + ChatColor.DARK_GRAY + "> " + ChatColor.WHITE + e.getMessage());
             }
+            
         }
     }
 }
