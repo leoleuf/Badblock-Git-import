f2037365a9b1b6719d710dfb131e1697ea832539
Huyka <antoine.mille@yahoo.com>
2019-11-03 22:41:11 +0100

    GameListeners modifications

--- src/main/java/fr/badblock/gameapi/listeners/GameListeners.java
+++ src/main/java/fr/badblock/gameapi/listeners/GameListeners.java
@@ -3,6 +3,9 @@ package fr.badblock.gameapi.listeners;
 import fr.badblock.gameapi.AbstractGameHandler;
 import fr.badblock.gameapi.Game;
 import fr.badblock.gameapi.GameAPI;
+import fr.badblock.gameapi.events.PlayerJoinGameEvent;
+import fr.badblock.gameapi.events.PlayerQuitGameEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -26,9 +29,8 @@ public class GameListeners implements Listener {
         Player player = event.getPlayer();
         game.getCache().add(player.getUniqueId());
         event.setJoinMessage(null);
-        AbstractGameHandler handler = game.getHandler();
-        handler.onPlayerJoin(player);
-        handler.startGame();
+        game.getHandler().startGame();
+        Bukkit.getPluginManager().callEvent(new PlayerJoinGameEvent(player, game));
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
@@ -38,7 +40,7 @@ public class GameListeners implements Listener {
         Player player = event.getPlayer();
         game.getCache().remove(player.getUniqueId());
         event.setQuitMessage(null);
-        game.getHandler().onPlayerQuit(player);
+        Bukkit.getPluginManager().callEvent(new PlayerQuitGameEvent(player, game));
     }
 
 }
